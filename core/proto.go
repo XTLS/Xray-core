4d11dc871ef5dd6c4e1ac40e14f572e84e21f8fd
JimhHan <50871214+JimhHan@users.noreply.github.com>
2020-12-09 22:55:14 +0800

    fix: proto code generate
     - use latest version of protoc-gen-gofast
     - generate code of addons.proto

--- core/proto.go
+++ core/proto.go
@@ -4,6 +4,7 @@ import "path/filepath"
 
 //go:generate go install -v google.golang.org/protobuf/cmd/protoc-gen-go
 //go:generate go install -v google.golang.org/grpc/cmd/protoc-gen-go-grpc
+//go:generate go get -v -u github.com/gogo/protobuf/protoc-gen-gofast
 //go:generate go install -v github.com/gogo/protobuf/protoc-gen-gofast
 //go:generate go run ../infra/vprotogen/main.go -pwd ./..
 
