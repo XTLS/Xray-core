f073456ac0f112e2412f9b06b5648d49bd9fecf5
秋のかえで <autmaple@protonmail.com>
2020-12-25 03:11:32 +0800

    Add TOML Support (#98)

--- infra/conf/serial/loader.go
+++ infra/conf/serial/loader.go
@@ -4,7 +4,9 @@ import (
 	"bytes"
 	"encoding/json"
 	"io"
+	"io/ioutil"
 
+	"github.com/BurntSushi/toml"
 	"github.com/xtls/xray-core/common/errors"
 	"github.com/xtls/xray-core/core"
 	"github.com/xtls/xray-core/infra/conf"
@@ -76,6 +78,41 @@ func LoadJSONConfig(reader io.Reader) (*core.Config, error) {
 	pbConfig, err := jsonConfig.Build()
 	if err != nil {
 		return nil, newError("failed to parse json config").Base(err)
+	}
+
+	return pbConfig, nil
+}
+
+func DecodeTOMLConfig(reader io.Reader) (*conf.Config, error) {
+	tomlFile, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return nil, newError("failed to read config file").Base(err)
+	}
+
+	configMap := make(map[string]interface{})
+
+	if _, err := toml.Decode(string(tomlFile), &configMap); err != nil {
+		return nil, newError("failed to convert TOML to Map").Base(err)
+	}
+
+	jsonFile, err := json.Marshal(&configMap)
+
+	if err != nil {
+		return nil, newError("failed to convert Map to JSON").Base(err)
+	}
+
+	return DecodeJSONConfig(bytes.NewReader(jsonFile))
+}
+
+func LoadTOMLConfig(reader io.Reader) (*core.Config, error) {
+	tomlConfig, err := DecodeTOMLConfig(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	pbConfig, err := tomlConfig.Build()
+	if err != nil {
+		return nil, newError("failed to parse toml config").Base(err)
 	}
 
 	return pbConfig, nil
