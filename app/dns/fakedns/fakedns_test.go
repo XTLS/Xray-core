a9d4005634db2ba92a5043cd2eecb9b62aa5af3d
RPRX <63339210+rprx@users.noreply.github.com>
2021-03-14 13:13:52 +0000

    undo fakedns_test.go

--- app/dns/fakedns/fakedns_test.go
+++ app/dns/fakedns/fakedns_test.go
@@ -18,7 +18,7 @@ func TestFakeDnsHolderCreateMapping(t *testing.T) {
 	fkdns, err := NewFakeDNSHolder()
 	common.Must(err)
 
-	addr := fkdns.GetFakeIPForDomain("fakednstest.example.com")
+	addr := fkdns.GetFakeIPForDomain("fakednstest.v2fly.org")
 	assert.Equal(t, "240.", addr[0].IP().String()[0:4])
 }
 
@@ -26,10 +26,10 @@ func TestFakeDnsHolderCreateMappingMany(t *testing.T) {
 	fkdns, err := NewFakeDNSHolder()
 	common.Must(err)
 
-	addr := fkdns.GetFakeIPForDomain("fakednstest.example.com")
+	addr := fkdns.GetFakeIPForDomain("fakednstest.v2fly.org")
 	assert.Equal(t, "240.", addr[0].IP().String()[0:4])
 
-	addr2 := fkdns.GetFakeIPForDomain("fakednstest2.example.com")
+	addr2 := fkdns.GetFakeIPForDomain("fakednstest2.v2fly.org")
 	assert.Equal(t, "240.", addr2[0].IP().String()[0:4])
 	assert.NotEqual(t, addr[0].IP().String(), addr2[0].IP().String())
 }
@@ -38,17 +38,17 @@ func TestFakeDnsHolderCreateMappingManyAndResolve(t *testing.T) {
 	fkdns, err := NewFakeDNSHolder()
 	common.Must(err)
 
-	addr := fkdns.GetFakeIPForDomain("fakednstest.example.com")
-	addr2 := fkdns.GetFakeIPForDomain("fakednstest2.example.com")
+	addr := fkdns.GetFakeIPForDomain("fakednstest.v2fly.org")
+	addr2 := fkdns.GetFakeIPForDomain("fakednstest2.v2fly.org")
 
 	{
 		result := fkdns.GetDomainFromFakeDNS(addr[0])
-		assert.Equal(t, "fakednstest.example.com", result)
+		assert.Equal(t, "fakednstest.v2fly.org", result)
 	}
 
 	{
 		result := fkdns.GetDomainFromFakeDNS(addr2[0])
-		assert.Equal(t, "fakednstest2.example.com", result)
+		assert.Equal(t, "fakednstest2.v2fly.org", result)
 	}
 }
 
@@ -56,8 +56,8 @@ func TestFakeDnsHolderCreateMappingManySingleDomain(t *testing.T) {
 	fkdns, err := NewFakeDNSHolder()
 	common.Must(err)
 
-	addr := fkdns.GetFakeIPForDomain("fakednstest.example.com")
-	addr2 := fkdns.GetFakeIPForDomain("fakednstest.example.com")
+	addr := fkdns.GetFakeIPForDomain("fakednstest.v2fly.org")
+	addr2 := fkdns.GetFakeIPForDomain("fakednstest.v2fly.org")
 	assert.Equal(t, addr[0].IP().String(), addr2[0].IP().String())
 }
 
@@ -72,23 +72,23 @@ func TestFakeDnsHolderCreateMappingAndRollOver(t *testing.T) {
 
 	common.Must(err)
 
-	addr := fkdns.GetFakeIPForDomain("fakednstest.example.com")
-	addr2 := fkdns.GetFakeIPForDomain("fakednstest2.example.com")
+	addr := fkdns.GetFakeIPForDomain("fakednstest.v2fly.org")
+	addr2 := fkdns.GetFakeIPForDomain("fakednstest2.v2fly.org")
 
 	for i := 0; i <= 8192; i++ {
 		{
 			result := fkdns.GetDomainFromFakeDNS(addr[0])
-			assert.Equal(t, "fakednstest.example.com", result)
+			assert.Equal(t, "fakednstest.v2fly.org", result)
 		}
 
 		{
 			result := fkdns.GetDomainFromFakeDNS(addr2[0])
-			assert.Equal(t, "fakednstest2.example.com", result)
+			assert.Equal(t, "fakednstest2.v2fly.org", result)
 		}
 
 		{
 			uuid := uuid.New()
-			domain := uuid.String() + ".fakednstest.example.com"
+			domain := uuid.String() + ".fakednstest.v2fly.org"
 			tempAddr := fkdns.GetFakeIPForDomain(domain)
 			rsaddr := tempAddr[0].IP().String()
 
