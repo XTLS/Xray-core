3f3bdd78daaa9d0fea9e902910611f272314a3ec
AkinoKaede <autmaple@protonmail.com>
2021-03-27 13:30:21 +0800

    feat: standard unit

--- common/bytes/bytesfmt.go
+++ common/bytes/bytesfmt.go
@@ -12,6 +12,10 @@ const (
 	KIBIBYTE
 	MEBIBYTE
 	GIBIBYTE
+
+	KILOBYTE = BYTE * 1000
+	MEGABYTE = KILOBYTE * 1000
+	GIGABYTE = MEGABYTE * 1000
 )
 
 var invalidByteQuantityError = newError("only accept byte like G/GB/GiB/Gbps M/MB/MiB/Mpbs, K/KB/KiB/Kbps, B/Bits").AtError()
@@ -34,22 +38,32 @@ func ToBytes(s string) (uint64, error) {
 	}
 
 	switch multiple {
-	case "g", "gbps":
-		return uint64(bytes * GIBIBYTE / 8), nil
-	case "gb", "gib", "gigabytes", "gibibytes":
+	case "g", "gbps", "gbits":
+		return uint64(bytes * GIGABYTE / 8), nil
+	case "gb", "gigabytes":
+		return uint64(bytes * GIGABYTE), nil
+	case "gib", "gibibytes":
 		return uint64(bytes * GIBIBYTE), nil
-	case "m", "mbps":
-		return uint64(bytes * MEBIBYTE / 8), nil
-	case "mb", "mib", "megabytes", "mebibytes":
+
+	case "m", "mbps", "mbits":
+		return uint64(bytes * MEGABYTE / 8), nil
+	case "mb", "megabytes":
+		return uint64(bytes * MEGABYTE), nil
+	case "mib", "mebibytes":
 		return uint64(bytes * MEBIBYTE), nil
-	case "k", "kbps":
-		return uint64(bytes * KIBIBYTE / 8), nil
-	case "kb", "kib", "kilobytes", "kibibytes":
+
+	case "k", "kbps", "kbits":
+		return uint64(bytes * KILOBYTE / 8), nil
+	case "kb", "kilobytes":
+		return uint64(bytes * KILOBYTE), nil
+	case "kib", "kibibytes":
 		return uint64(bytes * KIBIBYTE), nil
+
 	case "bps", "bits":
 		return uint64(bytes / 8), nil
 	case "b", "bytes":
 		return uint64(bytes), nil
+
 	default:
 		return 0, invalidByteQuantityError
 	}
