b8196a22a0ebdd3e66da846d019014c9b468e698
JimhHan <50871214+JimhHan@users.noreply.github.com>
2021-03-27 17:41:46 +0800

    Fix: matching type mismatch

--- common/matcher/geosite/geosite.go
+++ common/matcher/geosite/geosite.go
@@ -15,5 +15,19 @@ func ToDomains(dms []*Domain) []*domain.Domain {
 }
 
 func (d *Domain) ToDomain() *domain.Domain {
-	return &domain.Domain{Type: d.Type, Value: d.Value}
+	return &domain.Domain{Type: d.Type.ToMatchingType(), Value: d.Value}
+}
+
+func (t Domain_Type) ToMatchingType() domain.MatchingType {
+	switch t {
+	case Domain_Plain:
+		return domain.MatchingType_Keyword
+	case Domain_Regex:
+		return domain.MatchingType_Regex
+	case Domain_Domain:
+		return domain.MatchingType_Subdomain
+	case Domain_Full:
+		return domain.MatchingType_Full
+	}
+	panic("impossible")
 }
