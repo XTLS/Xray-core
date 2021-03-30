1bb3d006f36a27db66368fadf9d107a1484af57d
rurirei <72071920+rurirei@users.noreply.github.com>
2021-03-31 01:24:24 +0800

    fix: not return on bittorrent.Sniff

--- common/protocol/bittorrent/bittorrent.go
+++ common/protocol/bittorrent/bittorrent.go
@@ -22,10 +22,6 @@ var errNotBittorrent = errors.New("not bittorrent header")
 func SniffBittorrent(b []byte, shouldSniffDomain bool) (*SniffHeader, error) {
 	h := &SniffHeader{}
 
-	if !shouldSniffDomain {
-		return h, nil
-	}
-
 	if len(b) < 20 {
 		return nil, common.ErrNoClue
 	}
