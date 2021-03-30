ccd2ed74a0dedcd4dddde4ae71d113e6603e795a
rurirei <72071920+rurirei@users.noreply.github.com>
2021-03-31 01:21:42 +0800

    refine: return on bittorrent.Sniff

--- common/protocol/bittorrent/bittorrent.go
+++ common/protocol/bittorrent/bittorrent.go
@@ -20,12 +20,18 @@ func (h *SniffHeader) Domain() string {
 var errNotBittorrent = errors.New("not bittorrent header")
 
 func SniffBittorrent(b []byte, shouldSniffDomain bool) (*SniffHeader, error) {
+	h := &SniffHeader{}
+
+	if !shouldSniffDomain {
+		return h, nil
+	}
+
 	if len(b) < 20 {
 		return nil, common.ErrNoClue
 	}
 
 	if b[0] == 19 && string(b[1:20]) == "BitTorrent protocol" {
-		return &SniffHeader{}, nil
+		return h, nil
 	}
 
 	return nil, errNotBittorrent
