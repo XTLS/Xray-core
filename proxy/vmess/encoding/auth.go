ff35118af50d8eba8b5a3233ae0bd9dba6849642
Shelikhoo <xiaokangwang@outlook.com>
2021-04-29 06:29:42 +0800

    VMess AEAD based packet length
    (cherry picked from commit 08221600082a79376bdc262f2ffec1a3129ae98d)

--- proxy/vmess/encoding/auth.go
+++ proxy/vmess/encoding/auth.go
@@ -7,6 +7,8 @@ import (
 
 	"github.com/xtls/xray-core/common"
 
+	"github.com/xtls/xray-core/common/crypto"
+
 	"golang.org/x/crypto/sha3"
 )
 
@@ -115,4 +117,12 @@ func (s *ShakeSizeParser) NextPaddingLen() uint16 {
 
 func (s *ShakeSizeParser) MaxPaddingLen() uint16 {
 	return 64
+}
+
+type AEADSizeParser struct {
+	crypto.AEADChunkSizeParser
+}
+
+func NewAEADSizeParser(auth *crypto.AEADAuthenticator) *AEADSizeParser {
+	return &AEADSizeParser{crypto.AEADChunkSizeParser{Auth: auth}}
 }
