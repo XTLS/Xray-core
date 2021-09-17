e81271ab1c00909f6fbc56711580231770310edd
yuhan6665 <1588741+yuhan6665@users.noreply.github.com>
2021-09-16 20:40:05 -0400

    Update all proto files with existing vprotogen

--- core/proto.go
+++ core/proto.go
@@ -3,6 +3,7 @@ package core
 import "path/filepath"
 
 //go:generate go install -v google.golang.org/protobuf/cmd/protoc-gen-go
+//go:generate go get -v -u google.golang.org/grpc/cmd/protoc-gen-go-grpc
 //go:generate go install -v google.golang.org/grpc/cmd/protoc-gen-go-grpc
 //go:generate go get -v -u github.com/gogo/protobuf/protoc-gen-gofast
 //go:generate go install -v github.com/gogo/protobuf/protoc-gen-gofast
