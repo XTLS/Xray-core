f2cb13a8ecd4523392ce0fc1dca5737307ccc616
yuhan6665 <1588741+yuhan6665@users.noreply.github.com>
2021-09-20 02:46:05 -0400

    Deprecate legacy VMess header with a planned decommission (#712)
    * Deprecate legacy VMess header with a planned decommission
    * show legacy warning only once
    
    Co-authored-by: Xiaokang Wang <xiaokangwang@outlook.com>
    Co-authored-by: hmol233 <82594500+hmol233@users.noreply.github.com>

--- proxy/vmess/encoding/server.go
+++ proxy/vmess/encoding/server.go
@@ -191,10 +191,16 @@ func (s *ServerSession) DecodeRequestHeader(reader io.Reader, isDrain bool) (*pr
 		decryptor = bytes.NewReader(aeadData)
 		s.isAEADRequest = true
 
-	case !s.isAEADForced && errorAEAD == vmessaead.ErrNotFound:
+	case errorAEAD == vmessaead.ErrNotFound:
 		userLegacy, timestamp, valid, userValidationError := s.userValidator.Get(buffer.Bytes())
 		if !valid || userValidationError != nil {
 			return nil, drainConnection(newError("invalid user").Base(userValidationError))
+		}
+		if s.isAEADForced {
+			return nil, drainConnection(newError("invalid user: VMessAEAD is enforced and a non VMessAEAD connection is received. You can still disable this security feature with environment variable xray.vmess.aead.forced = false . You will not be able to enable legacy header workaround in the future."))
+		}
+		if s.userValidator.ShouldShowLegacyWarn() {
+			newError("Critical Warning: potentially invalid user: a non VMessAEAD connection is received. From 2022 Jan 1st, this kind of connection will be rejected by default. You should update or replace your client software now. This message will not be shown for further violation on this inbound.").AtWarning().WriteToLog()
 		}
 		user = userLegacy
 		iv := hashTimestamp(md5.New(), timestamp)
