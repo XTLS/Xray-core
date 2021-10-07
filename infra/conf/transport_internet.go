v2fly/v2ray-core

add json parse rule for tcp keep alive interval

infra/conf/transport_internet.go
@@ -387,6 +387,8 @@ type SocketConfig struct {
 	TFO                 *bool  `json:"tcpFastOpen"`
 	TProxy              string `json:"tproxy"`
 	AcceptProxyProtocol bool   `json:"acceptProxyProtocol"`
+
+	TcpKeepAliveInterval uint32 `json:"tcpKeepAliveInterval"`
 }
 
 // Build implements Buildable.
@@ -410,10 +412,11 @@ func (c *SocketConfig) Build() (*internet.SocketConfig, error) {
 	}
 
 	return &internet.SocketConfig{
-		Mark:                c.Mark,
-		Tfo:                 tfoSettings,
-		Tproxy:              tproxy,
-		AcceptProxyProtocol: c.AcceptProxyProtocol,
+		Mark:                 c.Mark,
+		Tfo:                  tfoSettings,
+		Tproxy:               tproxy,
+		AcceptProxyProtocol:  c.AcceptProxyProtocol,
+		TcpKeepAliveInterval: c.TcpKeepAliveInterval,
 	}, nil
 }
 

XTLS/Xray-core

infra/conf/transport_internet.go
@@ -489,6 +489,8 @@ type SocketConfig struct {
 	AcceptProxyProtocol bool        `json:"acceptProxyProtocol"`
 	DomainStrategy      string      `json:"domainStrategy"`
 	DialerProxy         string      `json:"dialerProxy"`
+
+	TcpKeepAliveInterval uint32 `json:"tcpKeepAliveInterval"`
 }
 
 // Build implements Buildable.
@@ -529,12 +531,13 @@ func (c *SocketConfig) Build() (*internet.SocketConfig, error) {
 	}
 
 	return &internet.SocketConfig{
-		Mark:                c.Mark,
-		Tfo:                 tfo,
-		Tproxy:              tproxy,
-		DomainStrategy:      dStrategy,
-		AcceptProxyProtocol: c.AcceptProxyProtocol,
-		DialerProxy:         c.DialerProxy,
+		Mark:                 c.Mark,
+		Tfo:                  tfo,
+		Tproxy:               tproxy,
+		DomainStrategy:       dStrategy,
+		AcceptProxyProtocol:  c.AcceptProxyProtocol,
+		DialerProxy:          c.DialerProxy,
+		TcpKeepAliveInterval: c.TcpKeepAliveInterval,
 	}, nil
 }
 