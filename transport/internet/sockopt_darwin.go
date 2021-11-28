e062af9c24608492dd1d582f520a1faea1430f28
Shelikhoo <xiaokangwang@outlook.com>
2021-11-28 21:27:28 +0000

    use sys/unix instead of syscall
    Suggestion from:
    https://github.com/v2fly/v2ray-core/pull/1395#issuecomment-974761647

--- transport/internet/sockopt_darwin.go
+++ transport/internet/sockopt_darwin.go
@@ -5,13 +5,10 @@ import (
 )
 
 const (
-	// TCP_FASTOPEN is the socket option on darwin for TCP fast open.
-	TCP_FASTOPEN = 0x105
 	// TCP_FASTOPEN_SERVER is the value to enable TCP fast open on darwin for server connections.
 	TCP_FASTOPEN_SERVER = 0x01
 	// TCP_FASTOPEN_CLIENT is the value to enable TCP fast open on darwin for client connections.
 	TCP_FASTOPEN_CLIENT = 0x02 // nolint: revive,stylecheck
-	TCP_KEEPINTVL       = 0x101 // nolint: golint,stylecheck
 )
 
 func applyOutboundSocketOptions(network string, address string, fd uintptr, config *SocketConfig) error {
