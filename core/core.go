35d5a7fe93c2aea8005f178d807402de146faf54
yuhan6665 <1588741+yuhan6665@users.noreply.github.com>
2022-08-27 22:50:07 -0400

    Update version 1.5.10

--- core/core.go
+++ core/core.go
@@ -18,7 +18,7 @@ import (
 )
 
 var (
-	version  = "1.5.9"
+	version  = "1.5.10"
 	build    = "Custom"
 	codename = "Xray, Penetrates Everything."
 	intro    = "A unified platform for anti-censorship."
