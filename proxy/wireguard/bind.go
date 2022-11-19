aba23d5bdd50fe6a57d12ac27b4eca52a3149b37
nanoda0523 <nanoda0523@users.noreply.github.com>
2022-11-19 00:29:54 +0000

    improve performance
    improve performance

--- proxy/wireguard/bind.go
+++ proxy/wireguard/bind.go
@@ -15,7 +15,7 @@ import (
 
 type netReadInfo struct {
 	// status
-	lock sync.Mutex
+	waiter sync.WaitGroup
 	// param
 	buff []byte
 	// result
@@ -25,62 +25,18 @@ type netReadInfo struct {
 }
 
 type netBind struct {
-	workers     int
-	dialer      internet.Dialer
-	connections map[conn.Endpoint]net.Conn
+	workers int
+	dialer  internet.Dialer
 
 	readQueue chan *netReadInfo
 }
 
 func (*netBind) ParseEndpoint(s string) (conn.Endpoint, error) {
-	e, err := netip.ParseAddrPort(s)
-	return asEndpoint(e), err
-}
-
-func (bind *netBind) Open(uport uint16) ([]conn.ReceiveFunc, uint16, error) {
-	bind.connections = make(map[conn.Endpoint]net.Conn)
-	bind.readQueue = make(chan *netReadInfo)
-
-	fun := func(buff []byte) (cap int, ep conn.Endpoint, err error) {
-		defer func() {
-			if r := recover(); r != nil {
-				cap = 0
-				ep = nil
-				err = errors.New("channel closed")
-			}
-		}()
-
-		r := &netReadInfo{
-			buff: buff,
-		}
-		r.lock.Lock()
-		bind.readQueue <- r
-		r.lock.Lock()
-		// fucking dumb way
-		r.lock.Unlock()
-		return r.bytes, r.endpoint, r.err
-	}
-	workers := bind.workers
-	if workers <= 0 {
-		workers = 1
-	}
-	arr := make([]conn.ReceiveFunc, workers)
-	for i := 0; i < workers; i++ {
-		arr[i] = fun
+	addrPort, err := netip.ParseAddrPort(s)
+	if err != nil {
+		return nil, err
 	}
 
-	return arr, uint16(uport), nil
-}
-
-func (bind *netBind) Close() error {
-	bind.connections = nil
-	if bind.readQueue != nil {
-		close(bind.readQueue)
-	}
-	return nil
-}
-
-func (bind *netBind) connectTo(addrPort netip.AddrPort, endpoint conn.Endpoint) (net.Conn, error) {
 	addr := addrPort.Addr()
 	var ip xnet.Address
 	if addr.Is4() {
@@ -96,13 +52,59 @@ func (bind *netBind) connectTo(addrPort netip.AddrPort, endpoint conn.Endpoint) 
 		Port:    xnet.Port(addrPort.Port()),
 		Network: xnet.Network_UDP,
 	}
-	c, err := bind.dialer.Dial(context.Background(), dst)
-	if err != nil {
-		return nil, err
-	}
-	bind.connections[endpoint] = c
 
-	go func(readQueue <-chan *netReadInfo, endpoint conn.Endpoint) {
+	return &netEndpoint{
+		dst: dst,
+	}, nil
+}
+
+func (bind *netBind) Open(uport uint16) ([]conn.ReceiveFunc, uint16, error) {
+	bind.readQueue = make(chan *netReadInfo)
+
+	fun := func(buff []byte) (cap int, ep conn.Endpoint, err error) {
+		defer func() {
+			if r := recover(); r != nil {
+				cap = 0
+				ep = nil
+				err = errors.New("channel closed")
+			}
+		}()
+
+		r := &netReadInfo{
+			buff: buff,
+		}
+		r.waiter.Add(1)
+		bind.readQueue <- r
+		r.waiter.Wait() // wait read goroutine done, or we will miss the result
+		return r.bytes, r.endpoint, r.err
+	}
+	workers := bind.workers
+	if workers <= 0 {
+		workers = 1
+	}
+	arr := make([]conn.ReceiveFunc, workers)
+	for i := 0; i < workers; i++ {
+		arr[i] = fun
+	}
+
+	return arr, uint16(uport), nil
+}
+
+func (bind *netBind) Close() error {
+	if bind.readQueue != nil {
+		close(bind.readQueue)
+	}
+	return nil
+}
+
+func (bind *netBind) connectTo(endpoint *netEndpoint) error {
+	c, err := bind.dialer.Dial(context.Background(), endpoint.dst)
+	if err != nil {
+		return err
+	}
+	endpoint.conn = c
+
+	go func(readQueue <-chan *netReadInfo, endpoint *netEndpoint) {
 		for {
 			v, ok := <-readQueue
 			if !ok {
@@ -112,39 +114,33 @@ func (bind *netBind) connectTo(addrPort netip.AddrPort, endpoint conn.Endpoint) 
 			v.bytes = i
 			v.endpoint = endpoint
 			v.err = err
-			v.lock.Unlock()
+			v.waiter.Done()
 			if err != nil && errors.Is(err, io.EOF) {
-				delete(bind.connections, endpoint)
+				endpoint.conn = nil
 				return
 			}
 		}
 	}(bind.readQueue, endpoint)
 
-	return c, nil
+	return nil
 }
 
 func (bind *netBind) Send(buff []byte, endpoint conn.Endpoint) error {
-	if bind.connections == nil {
-		return newError("bind not be open yet")
-	}
-
 	var err error
 
-	uconn, ok := bind.connections[endpoint]
+	nend, ok := endpoint.(*netEndpoint)
 	if !ok {
-		nend, ok := endpoint.(StdNetEndpoint)
-		if !ok {
-			return conn.ErrWrongEndpointType
-		}
-		addrPort := netip.AddrPort(nend)
+		return conn.ErrWrongEndpointType
+	}
 
-		uconn, err = bind.connectTo(addrPort, endpoint)
+	if nend.conn == nil {
+		err = bind.connectTo(nend)
 		if err != nil {
 			return err
 		}
 	}
 
-	_, err = uconn.Write(buff)
+	_, err = nend.conn.Write(buff)
 
 	return err
 }
@@ -153,48 +149,29 @@ func (bind *netBind) SetMark(mark uint32) error {
 	return nil
 }
 
-type StdNetEndpoint netip.AddrPort
-
-func (StdNetEndpoint) ClearSrc() {}
-
-func (e StdNetEndpoint) DstIP() netip.Addr {
-	return (netip.AddrPort)(e).Addr()
+type netEndpoint struct {
+	dst  xnet.Destination
+	conn net.Conn
 }
 
-func (e StdNetEndpoint) SrcIP() netip.Addr {
-	return netip.Addr{} // not supported
+func (netEndpoint) ClearSrc() {}
+
+func (e netEndpoint) DstIP() netip.Addr {
+	return netip.Addr{}
 }
 
-func (e StdNetEndpoint) DstToBytes() []byte {
-	b, _ := (netip.AddrPort)(e).MarshalBinary()
-	return b
+func (e netEndpoint) SrcIP() netip.Addr {
+	return netip.Addr{}
 }
 
-func (e StdNetEndpoint) DstToString() string {
-	return (netip.AddrPort)(e).String()
+func (e netEndpoint) DstToBytes() []byte {
+	return []byte{}
 }
 
-func (e StdNetEndpoint) SrcToString() string {
+func (e netEndpoint) DstToString() string {
 	return ""
 }
 
-// endpointPool contains a re-usable set of mapping from netip.AddrPort to Endpoint.
-// This exists to reduce allocations: Putting a netip.AddrPort in an Endpoint allocates,
-// but Endpoints are immutable, so we can re-use them.
-var endpointPool = sync.Pool{
-	New: func() any {
-		return make(map[netip.AddrPort]conn.Endpoint)
-	},
-}
-
-// asEndpoint returns an Endpoint containing ap.
-func asEndpoint(ap netip.AddrPort) conn.Endpoint {
-	m := endpointPool.Get().(map[netip.AddrPort]conn.Endpoint)
-	defer endpointPool.Put(m)
-	e, ok := m[ap]
-	if !ok {
-		e = conn.Endpoint(StdNetEndpoint(ap))
-		m[ap] = e
-	}
-	return e
+func (e netEndpoint) SrcToString() string {
+	return ""
 }
