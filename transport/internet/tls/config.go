03b8c094de7250c2724be8208f7253c1cb3622ce
Yue Yin <yinyue200@outlook.com>
2023-02-17 16:01:24 +0800

    Support SPKI Fingerprint Pinning
    Support SPKI Fingerprint Pinning for TLSObject

--- transport/internet/tls/config.go
+++ transport/internet/tls/config.go
@@ -266,6 +266,20 @@ func (c *Config) verifyPeerCert(rawCerts [][]byte, verifiedChains [][]*x509.Cert
 		}
 		return newError("peer cert is unrecognized: ", base64.StdEncoding.EncodeToString(hashValue))
 	}
+
+	if c.PinnedPeerCertificatePublicKeySha256 != nil {
+		for _, v := range verifiedChains {
+			for _, cert := range v {
+				publicHash := GenerateCertPublicKeyHash(cert)
+				for _, c := range c.PinnedPeerCertificatePublicKeySha256 {
+					if hmac.Equal(publicHash, c) {
+						return nil
+					}
+				}
+			}
+		}
+		return newError("peer public key is unrecognized.")
+	}
 	return nil
 }
 
