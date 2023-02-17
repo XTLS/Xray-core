e1cd1fd33ece9c9190efd43acc356400d129cc05
RPRX <63339210+RPRX@users.noreply.github.com>
2023-02-17 12:39:18 +0000

    Allow empty "shortId" (client side)

--- infra/conf/transport_internet.go
+++ infra/conf/transport_internet.go
@@ -637,9 +637,6 @@ func (c *REALITYConfig) Build() (proto.Message, error) {
 		if config.PublicKey, err = base64.RawURLEncoding.DecodeString(c.PublicKey); err != nil || len(config.PublicKey) != 32 {
 			return nil, newError(`invalid "publicKey": `, c.PublicKey)
 		}
-		if c.ShortId == "" {
-			return nil, newError(`empty "shortId"`)
-		}
 		config.ShortId = make([]byte, 8)
 		if _, err = hex.Decode(config.ShortId, []byte(c.ShortId)); err != nil {
 			return nil, newError(`invalid "shortId": `, c.ShortId)
