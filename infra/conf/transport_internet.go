7f9ba799aaf5d8fd86050f4c53c94a28330c089e
root <xqzr_pmj@icloud.com>
2023-04-29 04:06:30 +0800

    修改：     infra/conf/transport_internet.go 	修改：     transport/internet/config.pb.go 	修改：     transport/internet/config.proto 	修改：     transport/internet/sockopt_linux.go

--- infra/conf/transport_internet.go
+++ infra/conf/transport_internet.go
@@ -617,6 +617,7 @@ type SocketConfig struct {
 	TCPKeepAliveIdle     int32       `json:"tcpKeepAliveIdle"`
 	TCPCongestion        string      `json:"tcpCongestion"`
 	TCPWindowClamp       int32       `json:"tcpWindowClamp"`
+	TCPMaxSeg	     int32	 `json:"tcpMaxSeg"`
 	TCPUserTimeout       int32       `json:"tcpUserTimeout"`
 	V6only               bool        `json:"v6only"`
 	Interface            string      `json:"interface"`
@@ -670,6 +671,7 @@ func (c *SocketConfig) Build() (*internet.SocketConfig, error) {
 		TcpKeepAliveIdle:     c.TCPKeepAliveIdle,
 		TcpCongestion:        c.TCPCongestion,
 		TcpWindowClamp:       c.TCPWindowClamp,
+		TcpMaxSeg:	      c.TCPMaxSeg,
 		TcpUserTimeout:       c.TCPUserTimeout,
 		V6Only:               c.V6only,
 		Interface:            c.Interface,
