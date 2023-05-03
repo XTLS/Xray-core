9148c75a1f18a6c3346ab74a3fb370ad7f8d8eb8
rui0572 <125641819+rui0572@users.noreply.github.com>
2023-05-03 22:24:23 +0800

    统一 `domainStrategy` 行为.

--- infra/conf/freedom.go
+++ infra/conf/freedom.go
@@ -36,6 +36,20 @@ func (c *FreedomConfig) Build() (proto.Message, error) {
 		config.DomainStrategy = freedom.Config_USE_IP4
 	case "useip6", "useipv6", "use_ip6", "use_ipv6", "use_ip_v6", "use-ip6", "use-ipv6", "use-ip-v6":
 		config.DomainStrategy = freedom.Config_USE_IP6
+	case "useip46", "useipv4v6", "use_ip46", "use_ipv4v6":
+		config.DomainStrategy = freedom.Config_USE_IP46
+	case "useip64", "useipv6v4", "use_ip64", "use_ipv6v4":
+		config.DomainStrategy = freedom.Config_USE_IP64
+	case "forceip", "force_ip":
+		config.DomainStrategy = freedom.Config_FORCE_IP
+	case "forceip4", "forceipv4", "force_ip4", "force_ipv4":
+		config.DomainStrategy = freedom.Config_FORCE_IP4
+	case "forceip6", "forceipv6", "force_ip6", "force_ipv6":
+		config.DomainStrategy = freedom.Config_FORCE_IP6
+	case "forceip46", "forceipv4v6", "force_ip46", "force_ipv4v6":
+		config.DomainStrategy = freedom.Config_FORCE_IP46
+	case "forceip64", "forceipv6v4", "force_ip64", "force_ipv6v4":
+		config.DomainStrategy = freedom.Config_FORCE_IP64
 	}
 
 	if c.Fragment != nil {
