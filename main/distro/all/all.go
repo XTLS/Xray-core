c9f517108cd731056383ee21effd742a3e86f87c
yuhan6665 <1588741+yuhan6665@users.noreply.github.com>
2023-06-06 23:38:34 -0400

    Remove mtproto

--- main/distro/all/all.go
+++ main/distro/all/all.go
@@ -40,7 +40,6 @@ import (
 	_ "github.com/xtls/xray-core/proxy/freedom"
 	_ "github.com/xtls/xray-core/proxy/http"
 	_ "github.com/xtls/xray-core/proxy/loopback"
-	_ "github.com/xtls/xray-core/proxy/mtproto"
 	_ "github.com/xtls/xray-core/proxy/shadowsocks"
 	_ "github.com/xtls/xray-core/proxy/socks"
 	_ "github.com/xtls/xray-core/proxy/trojan"
