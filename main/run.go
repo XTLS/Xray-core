c6dc6962a0fb403556f1a7a65c84154e21c81f8e
flowerinsnow <flowerimsnow@hotmail.com>
2023-08-04 12:14:33 +0800

    Allow json-with-comments (jsonc) ext file

--- main/run.go
+++ main/run.go
@@ -113,13 +113,15 @@ func dirExists(file string) bool {
 func getRegepxByFormat() string {
 	switch strings.ToLower(*format) {
 	case "json":
-		return `^.+\.json$`
+		// return `^.+\.jsonc?$`
+		return `^.+\.(json|jsonc)$`
 	case "toml":
 		return `^.+\.toml$`
 	case "yaml", "yml":
 		return `^.+\.(yaml|yml)$`
 	default:
-		return `^.+\.(json|toml|yaml|yml)$`
+		// return `^.+\.(jsonc?|toml|yaml|yml)$`
+		return `^.+\.(json|jsonc|toml|yaml|yml)$`
 	}
 }
 
