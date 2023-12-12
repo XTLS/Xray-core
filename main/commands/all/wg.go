2da476eef426e7bbb42915aa2178ebb3cc5a553c
チセ <123655015+chise0713@users.noreply.github.com>
2023-12-12 09:52:10 +0800

    Command: Add `wg` for wireguard key generation (#2794)
    * Command: Add `wg` for wireguard key generation
    
    * Command: Merge `x25519` and `wg`

---
+++ main/commands/all/wg.go
@@ -0,0 +1,27 @@
+package all
+
+import (
+	"github.com/xtls/xray-core/main/commands/base"
+)
+
+var cmdWG = &base.Command{
+	UsageLine: `{{.Exec}} wg [-i "private key (base64.StdEncoding)"]`,
+	Short:     `Generate key pair for wireguard key exchange`,
+	Long: `
+Generate key pair for wireguard key exchange.
+
+Random: {{.Exec}} wg
+
+From private key: {{.Exec}} wg -i "private key (base64.StdEncoding)"
+`,
+}
+
+func init() {
+	cmdWG.Run = executeWG // break init loop
+}
+
+var input_wireguard = cmdWG.Flag.String("i", "", "")
+
+func executeWG(cmd *base.Command, args []string) {
+	Curve25519Genkey(true, *input_wireguard)
+}
