859112af87da6a1000f9f81f817d12af549bbb0b
nobody <nobody@nowhere.mars>
2024-01-10 09:58:52 +0800

    Refactor round-robin.

--- app/router/balancing.go
+++ app/router/balancing.go
@@ -2,7 +2,6 @@ package router
 
 import (
 	"context"
-	reflect "reflect"
 	sync "sync"
 
 	"github.com/xtls/xray-core/common/dice"
@@ -26,35 +25,20 @@ func (s *RandomStrategy) PickOutbound(tags []string) string {
 }
 
 type RoundRobinStrategy struct {
-	mu         sync.Mutex
-	tags       []string
-	index      int
-	roundRobin *RoundRobinStrategy
-}
-
-func NewRoundRobin(tags []string) *RoundRobinStrategy {
-	return &RoundRobinStrategy{
-		tags: tags,
-	}
-}
-func (r *RoundRobinStrategy) NextTag() string {
-	r.mu.Lock()
-	defer r.mu.Unlock()
-
-	tags := r.tags[r.index]
-	r.index = (r.index + 1) % len(r.tags)
-	return tags
+	mu    sync.Mutex
+	index int
 }
 
 func (s *RoundRobinStrategy) PickOutbound(tags []string) string {
-	if len(tags) == 0 {
+	n := len(tags)
+	if n == 0 {
 		panic("0 tags")
 	}
-	if s.roundRobin == nil || !reflect.DeepEqual(s.roundRobin.tags, tags) {
-		s.roundRobin = NewRoundRobin(tags)
-	}
-	tag := s.roundRobin.NextTag()
 
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	tag := tags[s.index%n]
+	s.index = (s.index + 1) % n
 	return tag
 }
 
