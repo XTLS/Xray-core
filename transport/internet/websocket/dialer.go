7bca80bc45d131ea9dddc0c970102c8eff702668
deorth-kku <deorth@outlook.com>
2024-02-04 16:41:14 +0800

    pickup dailer dropped ctx

--- transport/internet/websocket/dialer.go
+++ transport/internet/websocket/dialer.go
@@ -147,7 +147,7 @@ func dialWebSocket(ctx context.Context, dest net.Destination, streamSettings *in
 		header.Set("Sec-WebSocket-Protocol", base64.RawURLEncoding.EncodeToString(ed))
 	}
 
-	conn, resp, err := dialer.Dial(uri, header)
+	conn, resp, err := dialer.DialContext(ctx, uri, header)
 	if err != nil {
 		var reason string
 		if resp != nil {
