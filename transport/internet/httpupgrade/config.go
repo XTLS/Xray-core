021868afcaa93ea94fed323782807ce4232fef57
风扇滑翔翼 <45535409+Fangliding@users.noreply.github.com>
2024-04-07 15:53:34 +0000

    Fix HTTPUpgrade init

--- transport/internet/httpupgrade/config.go
+++ transport/internet/httpupgrade/config.go
@@ -1,5 +1,10 @@
 package httpupgrade
 
+import (
+	"github.com/xtls/xray-core/common"
+	"github.com/xtls/xray-core/transport/internet"
+)
+
 func (c *Config) GetNormalizedPath() string {
 	path := c.Path
 	if path == "" {
@@ -9,4 +14,10 @@ func (c *Config) GetNormalizedPath() string {
 		return "/" + path
 	}
 	return path
+}
+
+func init() {
+	common.Must(internet.RegisterProtocolConfigCreator(protocolName, func() interface{} {
+		return new(Config)
+	}))
 }
