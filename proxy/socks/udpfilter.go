2ac70a7aa88c31c95b34f091412a9a3aa69c8469
RPRX <63339210+RPRX@users.noreply.github.com>
2024-05-22 02:42:01 +0000

    Update udpfilter.go

--- proxy/socks/udpfilter.go
+++ proxy/socks/udpfilter.go
@@ -1,7 +1,7 @@
 package socks
 
 import (
-	"strings"
+	"net"
 	"sync"
 )
 
@@ -14,56 +14,18 @@ And drop UDP packets from unauthorized IP.
 After discussion, we believe it is not necessary to add a timeout mechanism to this filter.
 */
 
-type Filter interface {
-	Check(ip string) bool
-	Add(ip string) bool
-	Enabled() bool
-}
-
 type UDPFilter struct {
-	pool    sync.Map
-	enabled bool
+	ips sync.Map
 }
 
-func NewUDPFilter(isEnable AuthType) Filter {
-	enable := false
-	if isEnable == AuthType_PASSWORD {
-		enable = true
-	}
-	return &UDPFilter{
-		pool:    sync.Map{},
-		enabled: enable,
-	}
-}
-
-func (f *UDPFilter) Check(ip string) bool {
-	ip = stripPort(ip)
-	_, exists := f.pool.Load(ip)
-	return exists
-}
-
-func (f *UDPFilter) Add(ip string) bool {
-	ip = stripPort(ip)
-	f.pool.Store(ip, true)
+func (f *UDPFilter) Add(addr net.Addr) bool {
+	ip, _, _ := net.SplitHostPort(addr.String())
+	f.ips.Store(ip, true)
 	return true
 }
 
-func (f *UDPFilter) Enabled() bool {
-	return f.enabled
-}
-
-// conn.RemoteAddr().String() will return an address with a port like 11.45.1.4:1919
-// We just need 11.45.1.4
-func stripPort(ip string) string {
-	if strings.HasPrefix(ip, "[") {
-		end := strings.Index(ip, "]")
-		if end != -1 && end < len(ip)-1 && ip[end+1] == ':' {
-			return ip[:end+1]
-		}
-	} else {
-		if colon := strings.LastIndex(ip, ":"); colon != -1 {
-			return ip[:colon]
-		}
-	}
-	return ip
+func (f *UDPFilter) Check(addr net.Addr) bool {
+	ip, _, _ := net.SplitHostPort(addr.String())
+	_, ok := f.ips.Load(ip)
+	return ok
 }
