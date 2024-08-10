f650d87083e86296023ec5b6b00f0b8678a5b630
nobody <59990325+vrnobody@users.noreply.github.com>
2024-08-11 00:09:07 +0800

    Commands: Add convert with two sub-commands (#3661)
    * Add back convert-configs-to-protobuf command.
    
    * Add convert-typedMessage-to-json command.
    
    * Add -debug and -type arguments into convert.pb sub-command.
    
    ---------
    
    Co-authored-by: nobody <nobody@nowhere.mars>

---
+++ main/commands/all/convert/protobuf.go
@@ -0,0 +1,81 @@
+package convert
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/xtls/xray-core/common/cmdarg"
+	creflect "github.com/xtls/xray-core/common/reflect"
+	"github.com/xtls/xray-core/core"
+	"github.com/xtls/xray-core/main/commands/base"
+
+	"google.golang.org/protobuf/proto"
+)
+
+var cmdProtobuf = &base.Command{
+	CustomFlags: true,
+	UsageLine:   "{{.Exec}} convert pb [-debug] [-type] [json file] [json file] ...",
+	Short:       "Convert multiple json configs to protobuf",
+	Long: `
+Convert multiple json configs to protobuf.
+
+Arguments:
+
+	-d, -debug
+		Show mix.pb as json.
+		FOR DEBUGGING ONLY!
+		DO NOT PASS THIS OUTPUT TO XRAY-CORE!
+
+	-t, -type
+		Inject type information into debug output.
+
+Examples:
+
+    {{.Exec}} convert pb config.json c1.json c2.json c3.json > mix.pb
+	`,
+	Run: executeConvertConfigsToProtobuf,
+}
+
+func executeConvertConfigsToProtobuf(cmd *base.Command, args []string) {
+
+	var optDump bool
+	var optType bool
+
+	cmd.Flag.BoolVar(&optDump, "d", false, "")
+	cmd.Flag.BoolVar(&optDump, "debug", false, "")
+	cmd.Flag.BoolVar(&optType, "t", false, "")
+	cmd.Flag.BoolVar(&optType, "type", false, "")
+	cmd.Flag.Parse(args)
+
+	unnamedArgs := cmdarg.Arg{}
+	for _, v := range cmd.Flag.Args() {
+		unnamedArgs.Set(v)
+	}
+
+	if len(unnamedArgs) < 1 {
+		base.Fatalf("empty config list")
+	}
+
+	pbConfig, err := core.LoadConfig("auto", unnamedArgs)
+	if err != nil {
+		base.Fatalf(err.Error())
+	}
+
+	if optDump {
+		if j, ok := creflect.MarshalToJson(pbConfig, optType); ok {
+			fmt.Println(j)
+			return
+		} else {
+			base.Fatalf("failed to marshal proto config to json.")
+		}
+	}
+
+	bytesConfig, err := proto.Marshal(pbConfig)
+	if err != nil {
+		base.Fatalf("failed to marshal proto config: %s", err)
+	}
+
+	if _, err := os.Stdout.Write(bytesConfig); err != nil {
+		base.Fatalf("failed to write proto config: %s", err)
+	}
+}
