41d03d1856a5b16521610792601a20fb1195418e
チセ <123655015+chise0713@users.noreply.github.com>
2024-08-20 07:21:19 +0800

    Wireguard inbound: Fix no route when bind to interface (#3698)

--- proxy/wireguard/tun_linux.go
+++ proxy/wireguard/tun_linux.go
@@ -200,6 +200,9 @@ func createKernelTun(localAddresses []netip.Addr, mtu int, handler promiscuousMo
 		r := netlink.NewRule()
 		r.Table, r.Family, r.Src = ipv6TableIndex, unix.AF_INET6, addr.IPNet
 		out.rules = append(out.rules, r)
+		r = netlink.NewRule()
+		r.Table, r.Family, r.OifName = ipv6TableIndex, unix.AF_INET6, n
+		out.rules = append(out.rules, r)
 	}
 
 	for _, addr := range out.linkAddrs {
