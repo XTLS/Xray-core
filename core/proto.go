7a3509346a0e29721bb2ddf7857addd622b5674c
mmmray <142015632+mmmray@users.noreply.github.com>
2024-08-22 05:09:02 -0500

    bump protobuf packages

--- core/proto.go
+++ core/proto.go
@@ -1,5 +1,5 @@
 package core
 
-//go:generate go install -v google.golang.org/protobuf/cmd/protoc-gen-go@v1.34.1
-//go:generate go install -v google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.3.0
+//go:generate go install -v google.golang.org/protobuf/cmd/protoc-gen-go@v1.34.2
+//go:generate go install -v google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.5.1
 //go:generate go run ../infra/vprotogen/main.go -pwd ./..
