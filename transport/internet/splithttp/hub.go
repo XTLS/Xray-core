3bb7b646be70226081525f08b50f9951e1729dae
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-09-19 04:10:01 +0000

    Adds CORS response headers.

--- transport/internet/splithttp/hub.go
+++ transport/internet/splithttp/hub.go
@@ -193,6 +193,12 @@ func (h *requestHandler) ServeHTTP(writer http.ResponseWriter, request *http.Req
 			writer.Header().Set("Content-Type", "text/event-stream")
 		}
 
+		// CORS headers for the browser dialer to work
+		if h.config.CorsHeader {
+			writer.Header().Set("Access-Control-Allow-Origin", "*")
+			writer.Header().Set("Access-Control-Allow-Methods", "GET, POST")
+		}
+
 		h.config.WriteResponseHeader(writer)
 
 		writer.WriteHeader(http.StatusOK)
