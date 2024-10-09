99d85d603876487c384b933886253258525e1cd4
风扇滑翔翼 <Fangliding.fshxy@outlook.com>
2024-10-09 13:29:52 +0000

    TLS: Add capability to handle intermediate certs

--- transport/internet/tls/config_other.go
+++ transport/internet/tls/config_other.go
@@ -4,7 +4,11 @@
 package tls
 
 import (
+	"context"
 	"crypto/x509"
+	"embed"
+	"encoding/pem"
+	"path/filepath"
 	"sync"
 
 	"github.com/xtls/xray-core/common/errors"
@@ -26,6 +30,13 @@ func (c *rootCertsCache) load() (*x509.CertPool, error) {
 	pool, err := x509.SystemCertPool()
 	if err != nil {
 		return nil, err
+	}
+	intermediate_certs, err := loadIntermediateCerts()
+	if err != nil {
+		return nil, err
+	}
+	for _, cert := range intermediate_certs {
+		pool.AddCert(cert)
 	}
 	c.pool = pool
 	return pool, nil
@@ -52,4 +63,44 @@ func (c *Config) getCertPool() (*x509.CertPool, error) {
 		}
 	}
 	return pool, err
+}
+
+//go:embed intermediate_certs/*.crt
+var certFiles embed.FS
+
+func loadIntermediateCerts() ([]*x509.Certificate, error) {
+	var certs []*x509.Certificate
+
+	entries, err := certFiles.ReadDir("intermediate_certs")
+	if err != nil {
+		return nil, errors.New("failed to read intermediate_certs")
+	}
+
+	for _, entry := range entries {
+		if !entry.IsDir() {
+			content, err := certFiles.ReadFile(filepath.Join("intermediate_certs", entry.Name()))
+			if err != nil {
+				return nil, errors.New("failed to read intermediate cert for ", entry.Name())
+			}
+
+			block, _ := pem.Decode(content)
+			if block == nil || block.Type != "CERTIFICATE" {
+				return nil, errors.New("failed to decode intermediate cert for ", entry.Name())
+			}
+
+			cert, err := x509.ParseCertificate(block.Bytes)
+			if err != nil {
+				return nil, errors.New("failed to parse intermediate cert for ", entry.Name())
+			}
+
+			if _, err := cert.Verify(x509.VerifyOptions{}); err != nil {
+				errors.LogError(context.Background(), "failed to verify intermediate cert for ", entry.Name())
+				continue
+			}
+
+			certs = append(certs, cert)
+		}
+	}
+
+	return certs, nil
 }
