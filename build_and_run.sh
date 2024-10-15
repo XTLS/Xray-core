525e1aa6989493da2b4d1da8f25db24ee4cb7075
AmirDLT <amirhosseindolatkhah2000@gmail.com>
2024-10-15 12:39:41 +0000

    [tmp] committing for test purposes

--- build_and_run.sh
+++ build_and_run.sh
@@ -8,4 +8,4 @@ sudo cp -f monitor_config.json /usr/local/x-ui/bin/monitor_config.json
 
 echo "x-ray has been successfully copied"
 
-# /usr/local/x-ui/bin/xray-linux-amd64
+/usr/local/x-ui/bin/xray-linux-amd64
