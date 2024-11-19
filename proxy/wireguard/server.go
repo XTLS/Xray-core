3628ab9999939ff38a9d2c7fd445173d91614a73
风扇滑翔翼 <Fangliding.fshxy@outlook.com>
2024-11-19 09:32:34 +0000

    Fix routing content leak

--- proxy/wireguard/server.go
+++ proxy/wireguard/server.go
@@ -147,12 +147,16 @@ func (s *Server) forwardConnection(dest net.Destination, conn net.Conn) {
 	if s.info.inboundTag != nil {
 		ctx = session.ContextWithInbound(ctx, s.info.inboundTag)
 	}
-	if s.info.outboundTag != nil {
-		ctx = session.ContextWithOutbounds(ctx, []*session.Outbound{s.info.outboundTag})
-	}
-	if s.info.contentTag != nil {
-		ctx = session.ContextWithContent(ctx, s.info.contentTag)
-	}
+
+	// what the hell is this?
+	// useless and making bugs
+
+	//	if s.info.outboundTag != nil {
+	//		ctx = session.ContextWithOutbounds(ctx, []*session.Outbound{s.info.outboundTag})
+	//	}
+	//if s.info.contentTag != nil {
+	//	ctx = session.ContextWithContent(ctx, s.info.contentTag)
+	//}
 
 	link, err := s.info.dispatcher.Dispatch(ctx, dest)
 	if err != nil {
