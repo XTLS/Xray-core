c14cfd0a687946ecc958e874a8f1f4602b49c9e3
AmirhosseinDolatkhah <AmirhosseinDolatkhah2000@gmail.com>
2024-12-02 18:39:38 +0330

    [feat] limit maximum thread created by process function

--- monitor/periodic.go
+++ monitor/periodic.go
@@ -1,5 +1,10 @@
 package monitor
 
+import (
+	. "github.com/amirdlt/flex/util"
+	"time"
+)
+
 func init() {
 	//NewPeriodicJob(func() {
 	//	_, err := i.OnlineStatCol().DeleteMany(ctx, M{"last_connection": M{"$lte": time.Now().Add(-c.WindowSize)}})
@@ -9,4 +14,8 @@ func init() {
 	//	ips := Stream[any](ids).MapToString(func(v any) string { return fmt.Sprint(v) })
 	//
 	//}, c.WindowSize).Start()
+
+	NewPeriodicJob(func() {
+		i.ReportInfo(len(goroutineCountLimiterSemaphore), "goroutine count limiter semaphore length")
+	}, time.Minute)
 }
