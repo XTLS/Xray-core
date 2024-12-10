54f9584bd906af41d841d3edc4cc0f4f36240054
yuhan6665 <1588741+yuhan6665@users.noreply.github.com>
2024-12-10 11:37:40 -0500

    Revert "Add debug log"
    This reverts commit ce241cb82db230d70876f3e81da97862ab04fed2.

--- infra/conf/xray.go
+++ infra/conf/xray.go
@@ -576,7 +576,6 @@ func (c *Config) Build() (*core.Config, error) {
 			return nil, err
 		}
 		config.App = append(config.App, serial.ToTypedMessage(r))
-		errors.LogDebug(context.Background(), "Config append Observatory")
 	}
 
 	if c.BurstObservatory != nil {
@@ -585,7 +584,6 @@ func (c *Config) Build() (*core.Config, error) {
 			return nil, err
 		}
 		config.App = append(config.App, serial.ToTypedMessage(r))
-		errors.LogDebug(context.Background(), "Config append BurstObservatory")
 	}
 
 	var inbounds []InboundDetourConfig
