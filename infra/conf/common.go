480eac7235c2b0ad3d04de1f10389ea9a54606ac
yiguous <147401898+yiguous@users.noreply.github.com>
2025-01-01 18:16:36 +0800

    Config: Correctly marshal Int32Range to JSON (#4234)
    Fixes https://github.com/XTLS/libXray/issues/62

--- infra/conf/common.go
+++ infra/conf/common.go
@@ -2,6 +2,7 @@ package conf
 
 import (
 	"encoding/json"
+	"fmt"
 	"strconv"
 	"strings"
 
@@ -256,6 +257,18 @@ type Int32Range struct {
 	Right int32
 	From  int32
 	To    int32
+}
+
+func (v Int32Range) MarshalJSON() ([]byte, error) {
+	return json.Marshal(v.String())
+}
+
+func (v Int32Range) String() string {
+	if v.Left == v.Right {
+		return strconv.Itoa(int(v.Left))
+	} else {
+		return fmt.Sprintf("%d-%d", v.Left, v.Right)
+	}
 }
 
 func (v *Int32Range) UnmarshalJSON(data []byte) error {
