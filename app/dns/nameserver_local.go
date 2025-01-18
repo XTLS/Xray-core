7b92e1c82a996fba9978e4ce68d55aca6fcc9a69
风扇滑翔翼 <Fangliding.fshxy@outlook.com>
2025-01-18 17:55:11 +0000

    DNS: Implement queryStrategy for localhost DNS

--- app/dns/nameserver_local.go
+++ app/dns/nameserver_local.go
@@ -14,18 +14,41 @@ import (
 
 // LocalNameServer is an wrapper over local DNS feature.
 type LocalNameServer struct {
-	client *localdns.Client
+	client        *localdns.Client
+	queryStrategy QueryStrategy
 }
 
 const errEmptyResponse = "No address associated with hostname"
 
 // QueryIP implements Server.
 func (s *LocalNameServer) QueryIP(ctx context.Context, domain string, _ net.IP, option dns.IPOption, _ bool) (ips []net.IP, err error) {
+	option = ResolveIpOptionOverride(s.queryStrategy, option)
+	if !option.IPv4Enable && !option.IPv6Enable {
+		return nil, dns.ErrEmptyResponse
+	}
+
 	start := time.Now()
 	ips, err = s.client.LookupIP(domain, option)
 
 	if err != nil && strings.HasSuffix(err.Error(), errEmptyResponse) {
 		err = dns.ErrEmptyResponse
+	}
+
+	// If ipv4 or ipv6 is disabled, filter out the unwanted IPs
+	if err != nil && (!option.IPv4Enable || !option.IPv6Enable) {
+		var FilteredIPs []net.IP
+		for _, ip := range ips {
+			if option.IPv4Enable && len(ip) == net.IPv4len {
+				FilteredIPs = append(FilteredIPs, ip)
+			}
+			if option.IPv6Enable && len(ip) == net.IPv6len {
+				FilteredIPs = append(FilteredIPs, ip)
+			}
+		}
+		if len(FilteredIPs) == 0 {
+			err = dns.ErrEmptyResponse
+		}
+		ips = FilteredIPs
 	}
 
 	if len(ips) > 0 {
@@ -42,14 +65,15 @@ func (s *LocalNameServer) Name() string {
 }
 
 // NewLocalNameServer creates localdns server object for directly lookup in system DNS.
-func NewLocalNameServer() *LocalNameServer {
+func NewLocalNameServer(queryStrategy QueryStrategy) *LocalNameServer {
 	errors.LogInfo(context.Background(), "DNS: created localhost client")
 	return &LocalNameServer{
-		client: localdns.New(),
+		queryStrategy: queryStrategy,
+		client:        localdns.New(),
 	}
 }
 
 // NewLocalDNSClient creates localdns client object for directly lookup in system DNS.
 func NewLocalDNSClient() *Client {
-	return &Client{server: NewLocalNameServer()}
+	return &Client{server: NewLocalNameServer(QueryStrategy_USE_IP)}
 }
