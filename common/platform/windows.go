da7765959f67846d75072fe611e6dca411ce0961
RPRX <63339210+RPRX@users.noreply.github.com>
2025-03-24 12:51:22 +0000

    Update windows.go

--- common/platform/windows.go
+++ common/platform/windows.go
@@ -19,12 +19,13 @@ func GetToolLocation(file string) string {
 	return filepath.Join(toolPath, file+".exe")
 }
 
-// GetAssetLocation searches for `file` in the executable dir
+// GetAssetLocation searches for `file` in the env dir and the executable dir
 func GetAssetLocation(file string) string {
 	assetPath := NewEnvFlag(AssetLocation).GetValue(getExecutableDir)
 	return filepath.Join(assetPath, file)
 }
 
+// GetCertLocation searches for `file` in the env dir and the executable dir
 func GetCertLocation(file string) string {
 	certPath := NewEnvFlag(CertLocation).GetValue(getExecutableDir)
 	return filepath.Join(certPath, file)
