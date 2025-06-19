38a4abd8e17f72304a761cc695a6d0b5e14aa63d
jesus <valmak29@gmail.com>
2025-06-19 23:35:27 +0400

    chore: gen proto.

--- core/proto.go
+++ core/proto.go
@@ -1,5 +1,5 @@
 package core
 
-//go:generate go install -v google.golang.org/protobuf/cmd/protoc-gen-go@latest
+//go:generate go install -v google.golang.org/protobuf/cmd/protoc-gen-go@v1.35.1
 //go:generate go install -v google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 //go:generate go run ../infra/vprotogen/main.go -pwd ./..
