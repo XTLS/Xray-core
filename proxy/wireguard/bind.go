defe743b23a6581433fc3d0d198c8e7b977e2cac
xqzr <34030394+xqzr@users.noreply.github.com>
2025-07-23 20:06:40 +0800

    re Update bind.go

--- proxy/wireguard/bind.go
+++ proxy/wireguard/bind.go
@@ -230,9 +230,11 @@ func (e netEndpoint) SrcIP() netip.Addr {
 }
 
 func (e netEndpoint) DstToBytes() []byte {
-	var dat []byte = e.dst.Address.IP().To16()[:]
+	var dat []byte
 	if e.dst.Address.Family().IsIPv4() {
 		dat = e.dst.Address.IP().To4()[:]
+	} else {
+		dat = e.dst.Address.IP().To16()[:]
 	}
 	dat = append(dat, byte(e.dst.Port), byte(e.dst.Port>>8))
 	return dat
