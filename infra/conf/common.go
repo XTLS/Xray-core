b8e1b1d269360956277313774ed8cc6830e44e30
xqzr <34030394+xqzr@users.noreply.github.com>
2025-07-23 20:03:28 +0800

    re Update common.go

--- infra/conf/common.go
+++ infra/conf/common.go
@@ -358,7 +358,7 @@ func ParseRangeString(str string) (int, int, error) {
 		return 0, 0, nil
 	}
 	// for range value, like "114-514"
-	var pair []string 
+	var pair []string
 	// Process sth like "-114-514" "-1919--810"
 	if strings.HasPrefix(str, "-") {
 		pair = splitFromSecondDash(str)
