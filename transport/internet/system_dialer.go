88e76efee1d0ec7c07170f32f459e3e01788cda3
xqzr <34030394+xqzr@users.noreply.github.com>
2025-07-25 16:34:28 +0800

    -1

--- transport/internet/system_dialer.go
+++ transport/internet/system_dialer.go
@@ -97,6 +97,9 @@ func (d *DefaultSystemDialer) Dial(ctx context.Context, src net.Address, dest ne
 	if sockopt != nil && (sockopt.TcpKeepAliveInterval != 0 || sockopt.TcpKeepAliveIdle != 0) {
 		TCPKeepAliveConfig = gonet.KeepAliveConfig {
 			Enable:   false,
+			Idle:     -1,
+			Interval: -1,
+			Count:    -1,
 		}
 	}
 	dialer := &net.Dialer{
