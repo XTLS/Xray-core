4b693752c35993369132754b02c171d7cbedcee9
风扇滑翔翼 <Fangliding.fshxy@outlook.com>
2025-09-09 15:12:48 +0000

    Framework for pass-through dialers. Signed-off-by: 风扇滑翔翼 <Fangliding.fshxy@outlook.com>

--- transport/internet/browser_dialer/dialer.go
+++ transport/internet/browser_dialer/dialer.go
@@ -7,6 +7,8 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"net/http"
+	"strings"
+	"sync"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -18,10 +20,21 @@ import (
 //go:embed dialer.html
 var webpage []byte
 
+type pageWithConnMap struct {
+	UUID        string
+	ControlConn *websocket.Conn
+	ConnMap     map[string]chan *websocket.Conn
+}
+
+var globalConnMap = make(map[string]*pageWithConnMap)
+
+var globalConnMutex = &sync.Mutex{}
+
 type task struct {
-	Method string `json:"method"`
-	URL    string `json:"url"`
-	Extra  any    `json:"extra,omitempty"`
+	Method   string `json:"method"`
+	URL      string `json:"url"`
+	ConnUUID string `json:"connUUID"`
+	Extra    any    `json:"extra,omitempty"`
 }
 
 var conns chan *websocket.Conn
@@ -43,10 +56,41 @@ func init() {
 		webpage = bytes.ReplaceAll(webpage, []byte("csrfToken"), []byte(csrfToken))
 		conns = make(chan *websocket.Conn, 256)
 		go http.ListenAndServe(addr, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if r.URL.Path == "/websocket" {
+			if strings.HasPrefix(r.URL.Path, "/websocket") {
 				if r.URL.Query().Get("token") == csrfToken {
 					if conn, err := upgrader.Upgrade(w, r, nil); err == nil {
-						conns <- conn
+						pathParts := strings.Split(r.URL.Path, "/")
+						if len(pathParts) < 3 {
+							errors.LogError(context.Background(), "Browser dialer http upgrade unexpected error")
+						}
+						globalConnMutex.Lock()
+						pageUUID := pathParts[1]
+						connUUID := pathParts[2]
+						if connUUID == "control" {
+							page := &pageWithConnMap{
+								UUID:        pageUUID,
+								ControlConn: conn,
+								ConnMap:     make(map[string]chan *websocket.Conn),
+							}
+							globalConnMap[pageUUID] = page
+						} else {
+							if globalConnMap[pageUUID] == nil {
+								errors.LogError(context.Background(), "Browser dialer unexpected connection request")
+							} else {
+								c := globalConnMap[pageUUID].ConnMap[connUUID]
+								if c != nil {
+									select {
+									case c <- conn:
+									default:
+										errors.LogError(context.Background(), "Browser dialer http upgrade unexpected error")
+									}
+								} else {
+									conn.Close()
+									errors.LogError(context.Background(), "Orphan connection detected")
+								}
+							}
+						}
+						globalConnMutex.Unlock()
 					} else {
 						errors.LogError(context.Background(), "Browser dialer http upgrade unexpected error")
 					}
@@ -55,6 +99,7 @@ func init() {
 				w.Write(webpage)
 			}
 		}))
+		go monitor()
 	}
 }
 
@@ -67,9 +112,11 @@ type webSocketExtra struct {
 }
 
 func DialWS(uri string, ed []byte) (*websocket.Conn, error) {
+	UUID := uuid.New()
 	task := task{
-		Method: "WS",
-		URL:    uri,
+		Method:   "WS",
+		URL:      uri,
+		ConnUUID: UUID.String(),
 	}
 
 	if ed != nil {
@@ -108,20 +155,24 @@ func httpExtraFromHeaders(headers http.Header) *httpExtra {
 }
 
 func DialGet(uri string, headers http.Header) (*websocket.Conn, error) {
+	UUID := uuid.New()
 	task := task{
-		Method: "GET",
-		URL:    uri,
-		Extra:  httpExtraFromHeaders(headers),
+		Method:   "GET",
+		URL:      uri,
+		ConnUUID: UUID.String(),
+		Extra:    httpExtraFromHeaders(headers),
 	}
 
 	return dialTask(task)
 }
 
 func DialPost(uri string, headers http.Header, payload []byte) error {
+	UUID := uuid.New()
 	task := task{
-		Method: "POST",
-		URL:    uri,
-		Extra:  httpExtraFromHeaders(headers),
+		Method:   "POST",
+		URL:      uri,
+		ConnUUID: UUID.String(),
+		Extra:    httpExtraFromHeaders(headers),
 	}
 
 	conn, err := dialTask(task)
@@ -130,11 +181,6 @@ func DialPost(uri string, headers http.Header, payload []byte) error {
 	}
 
 	err = conn.WriteMessage(websocket.BinaryMessage, payload)
-	if err != nil {
-		return err
-	}
-
-	err = CheckOK(conn)
 	if err != nil {
 		return err
 	}
@@ -149,31 +195,59 @@ func dialTask(task task) (*websocket.Conn, error) {
 		return nil, err
 	}
 
+	globalConnMutex.Lock()
+	var ControlConn *websocket.Conn
+	var pageUUID string
+	// the order of iterating a map is random
+	for uuid, page := range globalConnMap {
+		ControlConn = page.ControlConn
+		pageUUID = uuid
+		break
+	}
+	if ControlConn == nil {
+		return nil, errors.New("no control connection available")
+	}
 	var conn *websocket.Conn
-	for {
-		conn = <-conns
-		if conn.WriteMessage(websocket.TextMessage, data) != nil {
-			conn.Close()
-		} else {
-			break
+	connChan := make(chan *websocket.Conn, 1)
+	globalConnMap[pageUUID].ConnMap[task.ConnUUID] = connChan
+	globalConnMutex.Unlock()
+	defer func() {
+		globalConnMutex.Lock()
+		if globalConnMap[pageUUID] != nil {
+			delete(globalConnMap[pageUUID].ConnMap, task.ConnUUID)
 		}
-	}
-	err = CheckOK(conn)
+		globalConnMutex.Unlock()
+	}()
+	err = ControlConn.WriteMessage(websocket.TextMessage, data)
 	if err != nil {
-		return nil, err
+		return nil, errors.New("failed to send task to control connection").Base(err)
 	}
-
-	return conn, nil
+	select {
+	case conn = <-connChan:
+		return conn, nil
+	case <-time.After(5 * time.Second):
+		return nil, errors.New("timeout waiting for connection")
+	}
 }
 
-func CheckOK(conn *websocket.Conn) error {
-	if _, p, err := conn.ReadMessage(); err != nil {
-		conn.Close()
-		return err
-	} else if s := string(p); s != "ok" {
-		conn.Close()
-		return errors.New(s)
+func monitor() {
+	for {
+		globalConnMutex.Lock()
+		newGlobalConnMap := make(map[string]*pageWithConnMap)
+		for pageUUID, page := range globalConnMap {
+			newGlobalConnMap[pageUUID] = page
+		}
+		globalConnMutex.Unlock()
+		for pageUUID, page := range newGlobalConnMap {
+			if err := page.ControlConn.WriteControl(websocket.PingMessage, []byte{}, time.Time{}); err != nil {
+				globalConnMutex.Lock()
+				page.ControlConn.Close()
+				if globalConnMap[pageUUID] == newGlobalConnMap[pageUUID] {
+					delete(globalConnMap, pageUUID)
+				}
+				globalConnMutex.Unlock()
+			}
+		}
+		time.Sleep(16 * time.Second)
 	}
-
-	return nil
 }
