74dc0fc607f3d4cac19f4e615d560685dff2e2da
RPRX <63339210+RPRX@users.noreply.github.com>
2026-02-03 20:56:01 +0000

    Update transport_internet.go

--- infra/conf/transport_internet.go
+++ infra/conf/transport_internet.go
@@ -752,8 +752,8 @@ func (c *TLSConfig) Build() (proto.Message, error) {
 		if time.Now().After(time.Date(2026, 6, 1, 0, 0, 0, 0, time.UTC)) {
 			return nil, errors.PrintRemovedFeatureError(`"allowInsecure"`, `"pinnedPeerCertSha256"`)
 		} else {
-			errors.LogWarning(context.Background(), `"allowInsecure" will be removed after 2026-06-01, please use "pinnedPeerCertSha256" instead`)
-			config.AllowInsecure = c.AllowInsecure
+			errors.LogWarning(context.Background(), `"allowInsecure" will be removed automatically after 2026-06-01, please use "pinnedPeerCertSha256"(pcs) and "verifyPeerCertByName"(vcn) instead, PLEASE CONTACT YOUR SERVICE PROVIDER`)
+			config.AllowInsecure = true
 		}
 	}
 	if c.PinnedPeerCertSha256 != "" {
