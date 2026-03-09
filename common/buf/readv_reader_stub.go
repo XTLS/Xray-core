6c636a798cd400025f627268945f27ce6f190c03
Fangliding <Fangliding.fshxy@outlook.com>
2026-03-10 00:08:08 +0800

    Fix wasm build

--- common/buf/readv_reader_stub.go
+++ common/buf/readv_reader_stub.go
@@ -6,10 +6,12 @@ package buf
 import (
 	"io"
 	"syscall"
+
+	"github.com/xtls/xray-core/features/stats"
 )
 
 const useReadv = false
 
-func NewReadVReader(reader io.Reader, rawConn syscall.RawConn) Reader {
+func NewReadVReader(reader io.Reader, rawConn syscall.RawConn, counter stats.Counter) Reader {
 	panic("not implemented")
 }
