d9632458febf342dcc902699b2acdd015187c968
kastov <yk@sent.com>
2026-03-09 15:25:37 +0300

    refactor: Remove cmdAllUsersOnlineInfo and update cmdOnlineStatsIpList to support retrieving all online users

--- main/commands/all/api/api.go
+++ main/commands/all/api/api.go
@@ -34,6 +34,5 @@ var CmdAPI = &base.Command{
 		cmdOnlineStats,
 		cmdOnlineStatsIpList,
 		cmdGetAllOnlineUsers,
-		cmdAllUsersOnlineInfo,
 	},
 }
--- main/commands/all/api/stats_online_ip_list.go
+++ main/commands/all/api/stats_online_ip_list.go
@@ -7,10 +7,11 @@ import (
 
 var cmdOnlineStatsIpList = &base.Command{
 	CustomFlags: true,
-	UsageLine:   "{{.Exec}} api statsonlineiplist [--server=127.0.0.1:8080] [-email '']",
+	UsageLine:   "{{.Exec}} api statsonlineiplist [--server=127.0.0.1:8080] [-email '' | -all]",
 	Short:       "Retrieve a user's online IP addresses and access times",
 	Long: `
 Retrieve the online IP addresses and corresponding access timestamps for a user from Xray.
+Use -all to retrieve all online users with their IPs and timestamps.
 
 Arguments:
 
@@ -23,9 +24,13 @@ Arguments:
 	-email
 		The user's email address.
 
+	-all
+		Retrieve all online users with their IPs and timestamps.
+
 Example:
 
 	{{.Exec}} {{.LongName}} --server=127.0.0.1:8080 -email "xray@love.com"
+	{{.Exec}} {{.LongName}} --server=127.0.0.1:8080 -all
 `,
 	Run: executeOnlineStatsIpList,
 }
@@ -33,12 +38,24 @@ Example:
 func executeOnlineStatsIpList(cmd *base.Command, args []string) {
 	setSharedFlags(cmd)
 	email := cmd.Flag.String("email", "", "")
+	all := cmd.Flag.Bool("all", false, "")
 	cmd.Flag.Parse(args)
-	statName := "user>>>" + *email + ">>>online"
 	conn, ctx, close := dialAPIServer()
 	defer close()
 
 	client := statsService.NewStatsServiceClient(conn)
+
+	if *all {
+		r := &statsService.GetAllOnlineUsersRequest{}
+		resp, err := client.GetAllUsersOnlineInfo(ctx, r)
+		if err != nil {
+			base.Fatalf("failed to get stats: %s", err)
+		}
+		showJSONResponse(resp)
+		return
+	}
+
+	statName := "user>>>" + *email + ">>>online"
 	r := &statsService.GetStatsRequest{
 		Name:   statName,
 		Reset_: false,
