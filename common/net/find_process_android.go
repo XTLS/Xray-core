4de578690df9e055fffccaa51b380150782b8a67
风扇滑翔翼 <Fangliding.fshxy@outlook.com>
2026-04-12 04:34:58 +0800

    unexport

--- common/net/find_process_android.go
+++ common/net/find_process_android.go
@@ -6,15 +6,15 @@ import (
 	"github.com/xtls/xray-core/common/errors"
 )
 
-var AndroidFindProcessFinder func(network, srcIP string, srcPort uint16, destIP string, destPort uint16) (int, string, string, error)
+var androidFindProcessFinder func(network, srcIP string, srcPort uint16, destIP string, destPort uint16) (int, string, string, error)
 
 func RegisterAndroidFindProcessFinder(f func(network, srcIP string, srcPort uint16, destIP string, destPort uint16) (int, string, string, error)) {
-	AndroidFindProcessFinder = f
+	androidFindProcessFinder = f
 }
 
 func FindProcess(network, srcIP string, srcPort uint16, destIP string, destPort uint16) (int, string, string, error) {
-	if AndroidFindProcessFinder != nil {
-		return AndroidFindProcessFinder(network, srcIP, srcPort, destIP, destPort)
+	if androidFindProcessFinder != nil {
+		return androidFindProcessFinder(network, srcIP, srcPort, destIP, destPort)
 	}
 	return 0, "", "", errors.New("android process lookup must be registered before use")
 }
