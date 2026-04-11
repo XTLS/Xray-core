015fdee1f80ce98872f62594ec75bf3aefca0c42
风扇滑翔翼 <Fangliding.fshxy@outlook.com>
2026-04-12 04:33:51 +0800

    api

--- common/net/find_process_android.go
+++ common/net/find_process_android.go
@@ -6,10 +6,15 @@ import (
 	"github.com/xtls/xray-core/common/errors"
 )
 
-var AndroidFindProcessImpl func(network, srcIP string, srcPort uint16, destIP string, destPort uint16) (int, string, string, error) = func(network, srcIP string, srcPort uint16, destIP string, destPort uint16) (int, string, string, error) {
-	return 0, "", "", errors.New("stub: process lookup is not implemented")
+var AndroidFindProcessFinder func(network, srcIP string, srcPort uint16, destIP string, destPort uint16) (int, string, string, error)
+
+func RegisterAndroidFindProcessFinder(f func(network, srcIP string, srcPort uint16, destIP string, destPort uint16) (int, string, string, error)) {
+	AndroidFindProcessFinder = f
 }
 
 func FindProcess(network, srcIP string, srcPort uint16, destIP string, destPort uint16) (int, string, string, error) {
-	return AndroidFindProcessImpl(network, srcIP, srcPort, destIP, destPort)
+	if AndroidFindProcessFinder != nil {
+		return AndroidFindProcessFinder(network, srcIP, srcPort, destIP, destPort)
+	}
+	return 0, "", "", errors.New("android process lookup must be registered before use")
 }
