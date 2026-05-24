df06d8f3df2046bc8c175f559d96d200eef8f262
Fangliding <Fangliding.fshxy@outlook.com>
2026-05-24 23:15:47 +0800

    dst addr

--- proxy/socks/protocol.go
+++ proxy/socks/protocol.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/binary"
 	"io"
+	gonet "net"
 
 	"github.com/xtls/xray-core/common"
 	"github.com/xtls/xray-core/common/buf"
@@ -208,6 +209,13 @@ func (s *ServerSession) handshake5(nMethod byte, reader io.Reader, writer net.Co
 		}
 		responsePort = net.Port(udpHub.LocalAddr().(*net.UDPAddr).Port)
 		tempUDPConn = NewTempUDPConn(udpHub, writer)
+		if !(request.Address.IP().IsUnspecified() && request.Port == 0) {
+			var udpRemote gonet.Addr = &gonet.UDPAddr{
+				IP:   request.Address.IP(),
+				Port: int(request.Port),
+			}
+			tempUDPConn.remote.Store(&udpRemote)
+		}
 	}
 	if err := writeSocks5Response(writer, statusSuccess, responseAddress, responsePort); err != nil {
 		common.CloseIfExists(tempUDPConn)
