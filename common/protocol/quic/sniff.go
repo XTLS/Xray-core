201bb222831e7a17e79904f12b3694094709f55b
风扇滑翔翼 <Fangliding.fshxy@outlook.com>
2026-08-29 13:50:23 +0800

    use map

--- common/protocol/quic/sniff.go
+++ common/protocol/quic/sniff.go
@@ -32,7 +32,7 @@ var (
 	errNotQuicInitial = errors.New("not initial packet")
 )
 
-type quicStructure struct {
+type quicVersionSpec struct {
 	ver         uint32
 	typeInitial byte
 	initialSalt []byte
@@ -40,26 +40,30 @@ type quicStructure struct {
 }
 
 var (
-	quicDraft29 = quicStructure{
+	quicDraft29 = quicVersionSpec{
 		ver:         0xff00001d,
 		typeInitial: 0b00,
 		initialSalt: []byte{0xaf, 0xbf, 0xec, 0x28, 0x99, 0x93, 0xd2, 0x4c, 0x9e, 0x97, 0x86, 0xf1, 0x9c, 0x61, 0x11, 0xe0, 0x43, 0x90, 0xa8, 0x99},
 		labelPrefix: "quic",
 	}
-	quicV1 = quicStructure{
+	quicV1 = quicVersionSpec{
 		ver:         0x1,
 		typeInitial: 0b00,
 		initialSalt: []byte{0x38, 0x76, 0x2c, 0xf7, 0xf5, 0x59, 0x34, 0xb3, 0x4d, 0x17, 0x9a, 0xe6, 0xa4, 0xc8, 0x0c, 0xad, 0xcc, 0xbb, 0x7f, 0x0a},
 		labelPrefix: "quic",
 	}
-	quicV2 = quicStructure{
+	quicV2 = quicVersionSpec{
 		ver:         0x6b3343cf,
 		typeInitial: 0b01,
 		initialSalt: []byte{0x0d, 0xed, 0xe3, 0xde, 0xf7, 0x00, 0xa6, 0xdb, 0x81, 0x93, 0x81, 0xbe, 0x6e, 0x26, 0x9d, 0xcb, 0xf9, 0xbd, 0x2e, 0xd9},
 		labelPrefix: "quicv2",
 	}
 
-	listQUICVersions = []*quicStructure{&quicDraft29, &quicV1, &quicV2}
+	quicVersionSpecMap = map[uint32]*quicVersionSpec{
+		quicDraft29.ver: &quicDraft29,
+		quicV1.ver:      &quicV1,
+		quicV2.ver:      &quicV2,
+	}
 )
 
 func SniffQUIC(b []byte) (*SniffHeader, error) {
@@ -93,15 +97,10 @@ func SniffQUIC(b []byte) (*SniffHeader, error) {
 		}
 
 		versionNumber := binary.BigEndian.Uint32(vb)
-		var s *quicStructure
-		for _, v := range listQUICVersions {
-			if v.ver == versionNumber {
-				s = v
-				break
-			}
-		}
-
-		if s == nil {
+		var s *quicVersionSpec
+		if v, ok := quicVersionSpecMap[versionNumber]; ok {
+			s = v
+		} else {
 			return nil, errNotQuic
 		}
 
