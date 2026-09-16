d7113128f64823afbb99013ca0adc7bebf4385aa
null <null>
2026-09-16 19:09:06 +0800

    udphop

--- transport/internet/finalmask/udphop/conn.go
+++ transport/internet/finalmask/udphop/conn.go
@@ -206,6 +206,7 @@ func (c *udpHopConn) WriteTo(p []byte, addr net.Addr) (n int, err error) {
 	_, err = c.cur.WriteTo(p, c.addr)
 	if err != nil {
 		errors.LogErrorInner(context.Background(), err, "send err")
+		return 0, err
 	}
 	return len(p), nil
 }
