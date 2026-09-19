cf0c59857e2a209cf912966d68a513e7ffaf5a18
null <null>
2026-09-19 21:06:45 +0800

    respManager & refine

--- transport/internet/finalmask/xdns/server.go
+++ transport/internet/finalmask/xdns/server.go
@@ -6,246 +6,10 @@ import (
 	"sync"
 	"time"
 
-	"github.com/xtls/xray-core/common"
 	"github.com/xtls/xray-core/common/errors"
 	"github.com/xtls/xray-core/common/net"
 	"golang.org/x/net/dns/dnsmessage"
 )
-
-const (
-	respTTL = fragTTL
-)
-
-type Resp struct {
-	msg      dnsmessage.Message
-	addr     net.Addr
-	clientID ClientID
-	domain   *Domain
-
-	edns0 uint16
-	cap   int
-}
-
-func NewResp(msg dnsmessage.Message, addr net.Addr, clientID ClientID, domain *Domain) *Resp {
-	if len(msg.Questions) != 1 {
-		return nil
-	}
-
-	opt := false
-	var edns0 uint16
-	for i := range msg.Additionals {
-		if msg.Additionals[i].Header.Type == dnsmessage.TypeOPT {
-			if (msg.Additionals[i].Header.TTL>>16)&0xFF != 0 {
-				return nil
-			}
-			if opt {
-				return nil
-			}
-			opt = true
-			edns0 = uint16(msg.Additionals[i].Header.Class)
-		}
-	}
-	if edns0 > 4096 {
-		return nil
-	}
-	if edns0 > 0 && edns0 < 512 {
-		edns0 = 512
-	}
-	size := min(max(int(edns0), 512), max(int(domain.edns0), 512))
-
-	left := size - 12 - int(msg.Questions[0].Name.Length) - 1 - 2 - 2
-	if edns0 > 0 {
-		left -= 1 + 2 + 2 + 4 + 2 + 0
-	}
-	cap := 0
-	switch msg.Questions[0].Type {
-	case dnsmessage.TypeA:
-		single := 2 + 2 + 2 + 4 + 2 + 4
-		n := left / single
-		cap = 4*n - n - 1
-	case dnsmessage.TypeCNAME:
-		single := 2 + 2 + 2 + 4 + 2 + domain.lenMax
-		n := left / single
-		cap = domain.cap*n - n - 1
-	case dnsmessage.TypeTXT:
-		left -= 2 + 2 + 2 + 4 + 2
-		single := 1 + 255
-		n := left / single
-		m := left % single
-		cap = 255*n - n
-		if m > 1 {
-			cap += m - 1
-		}
-	case dnsmessage.TypeAAAA:
-		single := 2 + 2 + 2 + 4 + 2 + 16
-		n := left / single
-		cap = 16*n - n - 1
-	}
-	if cap < 11+1 {
-		return nil
-	}
-
-	msg.Header = dnsmessage.Header{
-		ID:            msg.Header.ID,
-		Response:      true,
-		Authoritative: true,
-		RCode:         dnsmessage.RCodeSuccess,
-	}
-	msg.Answers = nil
-	msg.Authorities = nil
-	msg.Additionals = nil
-	return &Resp{
-		msg:      msg,
-		addr:     addr,
-		clientID: clientID,
-		domain:   domain,
-
-		edns0: edns0,
-		cap:   cap,
-	}
-}
-
-// TODO: use dnsmessage.Builder
-func (r *Resp) Append(out []byte, data []byte) []byte {
-	if len(data) == 0 || len(data) > r.cap {
-		panic(len(data))
-	}
-	msg := r.msg
-	switch r.msg.Questions[0].Type {
-	case dnsmessage.TypeA:
-		fragN := 1
-		if (len(data) - (4 - 2)) > 0 {
-			fragN += (len(data) - (4 - 2)) / (4 - 1)
-			if (len(data)-(4-2))%(4-1) > 0 {
-				fragN++
-			}
-		}
-
-		for i := range fragN {
-			A := [4]byte{byte(i)}
-			if i == 0 {
-				A[1] = byte(fragN)
-				n := copy(A[2:], data)
-				data = data[n:]
-			} else {
-				n := copy(A[1:], data)
-				data = data[n:]
-			}
-			msg.Answers = append(msg.Answers, dnsmessage.Resource{
-				Header: dnsmessage.ResourceHeader{
-					Name:  msg.Questions[0].Name,
-					Type:  msg.Questions[0].Type,
-					Class: dnsmessage.ClassINET,
-					TTL:   60,
-				},
-				Body: &dnsmessage.AResource{A: A},
-			})
-		}
-	case dnsmessage.TypeCNAME:
-		fragN := 1
-		if (len(data) - (r.domain.cap - 2)) > 0 {
-			fragN += (len(data) - (r.domain.cap - 2)) / (r.domain.cap - 1)
-			if (len(data)-(r.domain.cap-2))%(r.domain.cap-1) > 0 {
-				fragN++
-			}
-		}
-
-		DATA := make([]byte, r.domain.cap)
-		for i := range fragN {
-			DATA[0] = byte(i)
-			if i == 0 {
-				DATA[1] = byte(fragN)
-				n := copy(DATA[2:], data)
-				data = data[n:]
-				msg.Answers = append(msg.Answers, dnsmessage.Resource{
-					Header: dnsmessage.ResourceHeader{
-						Name:  msg.Questions[0].Name,
-						Type:  msg.Questions[0].Type,
-						Class: dnsmessage.ClassINET,
-						TTL:   60,
-					},
-					Body: &dnsmessage.CNAMEResource{CNAME: r.domain.Encode(DATA[:n+2])},
-				})
-			} else {
-				n := copy(DATA[1:], data)
-				data = data[n:]
-				msg.Answers = append(msg.Answers, dnsmessage.Resource{
-					Header: dnsmessage.ResourceHeader{
-						Name:  msg.Questions[0].Name,
-						Type:  msg.Questions[0].Type,
-						Class: dnsmessage.ClassINET,
-						TTL:   60,
-					},
-					Body: &dnsmessage.CNAMEResource{CNAME: r.domain.Encode(DATA[:n+1])},
-				})
-			}
-		}
-	case dnsmessage.TypeTXT:
-		var txt []string
-		for len(data) > 0 {
-			size := min(len(data), 255)
-			txt = append(txt, string(data[:size]))
-			data = data[size:]
-		}
-		msg.Answers = append(msg.Answers, dnsmessage.Resource{
-			Header: dnsmessage.ResourceHeader{
-				Name:  msg.Questions[0].Name,
-				Type:  msg.Questions[0].Type,
-				Class: dnsmessage.ClassINET,
-				TTL:   60,
-			},
-			Body: &dnsmessage.TXTResource{TXT: txt},
-		})
-	case dnsmessage.TypeAAAA:
-		fragN := 1
-		if (len(data) - (16 - 2)) > 0 {
-			fragN += (len(data) - (16 - 2)) / (16 - 1)
-			if (len(data)-(16-2))%(16-1) > 0 {
-				fragN++
-			}
-		}
-
-		for i := range fragN {
-			AAAA := [16]byte{byte(i)}
-			if i == 0 {
-				AAAA[1] = byte(fragN)
-				n := copy(AAAA[2:], data)
-				data = data[n:]
-			} else {
-				n := copy(AAAA[1:], data)
-				data = data[n:]
-			}
-			msg.Answers = append(msg.Answers, dnsmessage.Resource{
-				Header: dnsmessage.ResourceHeader{
-					Name:  msg.Questions[0].Name,
-					Type:  msg.Questions[0].Type,
-					Class: dnsmessage.ClassINET,
-					TTL:   60,
-				},
-				Body: &dnsmessage.AAAAResource{AAAA: AAAA},
-			})
-		}
-	}
-	if r.edns0 > 0 {
-		msg.Additionals = append(msg.Additionals, dnsmessage.Resource{
-			Header: dnsmessage.ResourceHeader{
-				Name:  dnsmessage.MustNewName("."),
-				Type:  dnsmessage.TypeOPT,
-				Class: dnsmessage.Class(r.edns0),
-				TTL:   0,
-			},
-			Body: &dnsmessage.OPTResource{},
-		})
-	}
-	return common.Must2(msg.AppendPack(out))
-}
-
-type RespInfo struct {
-	resp     chan *Resp
-	fragID   byte
-	capFrags int
-	deadline time.Time
-}
 
 type xdnsServer struct {
 	net.PacketConn
@@ -253,7 +17,7 @@ type xdnsServer struct {
 	domains      []*Domain
 	minAvailable int
 	fragManager  *FragManager
-	m            map[ClientID]RespInfo
+	respManager  *RespManager
 
 	readCh  chan packet
 	closeCh chan struct{}
@@ -286,7 +50,7 @@ func NewServer(c *Config, raw net.PacketConn) (net.PacketConn, error) {
 		domains:      domains,
 		minAvailable: int(c.MinAvailable),
 		fragManager:  NewFragManager(),
-		m:            make(map[ClientID]RespInfo),
+		respManager:  NewRespManager(),
 
 		readCh:  make(chan packet),
 		closeCh: make(chan struct{}),
@@ -302,73 +66,6 @@ func (c *xdnsServer) closed() bool {
 	default:
 		return false
 	}
-}
-
-func (c *xdnsServer) push(clientID ClientID, resp *Resp) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	if c.closed() {
-		return
-	}
-	now := time.Now()
-	info, ok := c.m[clientID]
-	if !ok || now.After(info.deadline) {
-		info = RespInfo{
-			resp:     make(chan *Resp, 255),
-			deadline: now.Add(respTTL),
-		}
-	}
-	select {
-	case info.resp <- resp:
-		info.capFrags += resp.cap - 11
-	default:
-		r := <-info.resp
-		info.capFrags -= r.cap - 11
-		info.resp <- resp
-		info.capFrags += resp.cap - 11
-	}
-	c.m[clientID] = info
-}
-
-func (c *xdnsServer) pop(clientID ClientID, lenp int) ([]*Resp, byte) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	if c.closed() {
-		return nil, 0
-	}
-	now := time.Now()
-	info, ok := c.m[clientID]
-	if !ok || now.After(info.deadline) {
-		if ok {
-			delete(c.m, clientID)
-		}
-		return nil, 0
-	}
-	if len(info.resp) < c.minAvailable || info.capFrags < lenp {
-		return nil, 0
-	}
-	var resps []*Resp
-	size := 0
-	for {
-		r := <-info.resp
-		info.capFrags -= r.cap - 11
-		resps = append(resps, r)
-		size += r.cap - 11
-		if len(resps) == 0 && lenp <= r.cap-8 {
-			break
-		}
-		if lenp <= size {
-			break
-		}
-	}
-	fragID := byte(0)
-	if len(resps) > 1 {
-		fragID = info.fragID
-		info.fragID++
-	}
-	info.deadline = now.Add(respTTL)
-	c.m[clientID] = info
-	return resps, fragID
 }
 
 func (c *xdnsServer) read(buf []byte, addr net.Addr) {
@@ -405,7 +102,7 @@ func (c *xdnsServer) read(buf []byte, addr net.Addr) {
 	if resp == nil {
 		return
 	}
-	c.push(clientID, resp)
+	c.respManager.Push(clientID, resp)
 
 	if decoded[8]&0x80 == 0x80 {
 		return
@@ -436,7 +133,7 @@ func (c *xdnsServer) read(buf []byte, addr net.Addr) {
 
 func (c *xdnsServer) send(p []byte, addr net.Addr) {
 	clientID := ClientIDFromAddr(addr.(*net.UDPAddr))
-	resps, fragID := c.pop(clientID, len(p))
+	resps, fragID := c.respManager.Pop(clientID, len(p), c.minAvailable)
 	if len(resps) == 0 {
 		return
 	}
@@ -466,36 +163,13 @@ func (c *xdnsServer) send(p []byte, addr net.Addr) {
 }
 
 func (c *xdnsServer) run() {
-	go c.gc()
-
 	c.wg.Add(1)
 	go c.recv()
 
 	c.wg.Wait()
 	close(c.readCh)
 	c.fragManager.Close()
-	for key := range c.m {
-		delete(c.m, key)
-	}
-}
-
-func (c *xdnsServer) gc() {
-	ticker := time.NewTicker(respTTL / 2)
-	defer ticker.Stop()
-	for {
-		select {
-		case <-c.closeCh:
-			return
-		case now := <-ticker.C:
-			c.mu.Lock()
-			for key, info := range c.m {
-				if now.After(info.deadline) {
-					delete(c.m, key)
-				}
-			}
-			c.mu.Unlock()
-		}
-	}
+	c.respManager.Close()
 }
 
 func (c *xdnsServer) recv() {
