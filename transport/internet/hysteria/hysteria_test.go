42c97bad8ed7c9cd67952fce372afa63ba29b0b0
null <null>
2026-09-21 01:56:46 +0800

    datagram test

--- transport/internet/hysteria/hysteria_test.go
+++ transport/internet/hysteria/hysteria_test.go
@@ -16,9 +16,7 @@ import (
 )
 
 func TestDatagram(t *testing.T) {
-	run := func() (addr net.Addr, done chan struct{}) {
-		done = make(chan struct{})
-
+	run := func() (addr net.Addr, recv chan int64, cancel func()) {
 		cert, _ := cert.MustGenerate(nil)
 		Certificate := [][]byte{cert.Certificate}
 		PrivateKey := common.Must2(x509.ParsePKCS8PrivateKey(cert.PrivateKey))
@@ -51,58 +49,117 @@ func TestDatagram(t *testing.T) {
 		tr := &quic.Transport{Conn: pktConn}
 		l := common.Must2(tr.Listen(tlsConf, quicConf))
 
+		recv = make(chan int64)
+		ctx, cancel := context.WithCancel(context.Background())
+
 		go func() {
+			defer pktConn.Close()
+			defer tr.Close()
 			defer l.Close()
-			conn := common.Must2(l.Accept(context.Background()))
-			defer conn.CloseWithError(0, "")
+			defer close(recv)
+
 			var buf [1500]byte
-			err := conn.SendDatagram(buf[:])
-			var qErr *quic.DatagramTooLargeError
-			if !errors.As(err, &qErr) || qErr.MaxDatagramPayloadSize != 1197 {
-				t.Fatal(err)
+			for {
+				conn, err := l.Accept(ctx)
+				if err != nil {
+					if !errors.Is(err, context.Canceled) {
+						t.Error(err)
+					}
+					break
+				}
+				err = conn.SendDatagram(buf[:])
+				var qErr *quic.DatagramTooLargeError
+				if !errors.As(err, &qErr) {
+					t.Error(err)
+				}
+				recv <- qErr.MaxDatagramPayloadSize
+				defer conn.CloseWithError(0, "")
 			}
-			<-done
 		}()
 
-		return l.Addr(), done
+		return l.Addr(), recv, cancel
 	}
 
-	addr, done := run()
+	addr, recv, cancel := run()
 
-	tlsConf := &tls.Config{
-		InsecureSkipVerify: true,
-	}
+	t.Run("With ChromeParrot", func(t *testing.T) {
+		tlsConf := &tls.Config{
+			InsecureSkipVerify: true,
+		}
 
-	quicConf := &quic.Config{
-		InitialStreamReceiveWindow:     8388608,
-		MaxStreamReceiveWindow:         8388608,
-		InitialConnectionReceiveWindow: 8388608 * 5 / 2,
-		MaxConnectionReceiveWindow:     8388608 * 5 / 2,
-		MaxIdleTimeout:                 30 * time.Second,
-		KeepAlivePeriod:                10 * time.Second,
-		DisablePathMTUDiscovery:        runtime.GOOS != "linux" && runtime.GOOS != "windows" && runtime.GOOS != "darwin",
-		ChromeParrot:                   true,
-		EnableDatagrams:                true,
-		MaxDatagramFrameSize:           MaxDatagramFrameSize,
-		OmitMaxDatagramFrameSize:       true,
-		DisablePathManager:             true,
-	}
+		quicConf := &quic.Config{
+			InitialStreamReceiveWindow:     8388608,
+			MaxStreamReceiveWindow:         8388608,
+			InitialConnectionReceiveWindow: 8388608 * 5 / 2,
+			MaxConnectionReceiveWindow:     8388608 * 5 / 2,
+			MaxIdleTimeout:                 30 * time.Second,
+			KeepAlivePeriod:                10 * time.Second,
+			DisablePathMTUDiscovery:        runtime.GOOS != "linux" && runtime.GOOS != "windows" && runtime.GOOS != "darwin",
+			ChromeParrot:                   true,
+			EnableDatagrams:                true,
+			MaxDatagramFrameSize:           MaxDatagramFrameSize,
+			OmitMaxDatagramFrameSize:       true,
+			DisablePathManager:             true,
+		}
 
-	pktConn := common.Must2(net.ListenPacket("udp", "127.0.0.1:0"))
-	defer pktConn.Close()
-	tr := &quic.Transport{Conn: pktConn, ConnectionIDGenerator: quic.ZeroLengthConnectionIDGenerator{}}
-	defer tr.Close()
+		pktConn := common.Must2(net.ListenPacket("udp", "127.0.0.1:0"))
+		tr := &quic.Transport{Conn: pktConn, ConnectionIDGenerator: quic.ZeroLengthConnectionIDGenerator{}}
+		conn := common.Must2(tr.DialEarly(context.Background(), addr, tlsConf, quicConf))
 
-	conn := common.Must2(tr.DialEarly(context.Background(), addr, tlsConf, quicConf))
-	defer conn.CloseWithError(0, "")
+		defer pktConn.Close()
+		defer tr.Close()
+		defer conn.CloseWithError(0, "")
 
-	var buf [1500]byte
-	err := conn.SendDatagram(buf[:])
+		var buf [1500]byte
+		err := conn.SendDatagram(buf[:])
+		var qErr *quic.DatagramTooLargeError
+		if !errors.As(err, &qErr) || qErr.MaxDatagramPayloadSize != 1197 {
+			t.Error(err)
+		}
+		if server := <-recv; server != 1243 {
+			t.Error(server)
+		}
+	})
 
-	close(done)
+	t.Run("Without ChromeParrot", func(t *testing.T) {
+		tlsConf := &tls.Config{
+			InsecureSkipVerify: true,
+			NextProtos:         []string{"h3"},
+		}
 
-	var qErr *quic.DatagramTooLargeError
-	if !errors.As(err, &qErr) || qErr.MaxDatagramPayloadSize != 1197 {
-		t.Fatal(err)
-	}
+		quicConf := &quic.Config{
+			InitialStreamReceiveWindow:     8388608,
+			MaxStreamReceiveWindow:         8388608,
+			InitialConnectionReceiveWindow: 8388608 * 5 / 2,
+			MaxConnectionReceiveWindow:     8388608 * 5 / 2,
+			MaxIdleTimeout:                 30 * time.Second,
+			KeepAlivePeriod:                10 * time.Second,
+			DisablePathMTUDiscovery:        runtime.GOOS != "linux" && runtime.GOOS != "windows" && runtime.GOOS != "darwin",
+			ChromeParrot:                   false,
+			EnableDatagrams:                true,
+			MaxDatagramFrameSize:           MaxDatagramFrameSize,
+			OmitMaxDatagramFrameSize:       true,
+			DisablePathManager:             true,
+		}
+
+		pktConn := common.Must2(net.ListenPacket("udp", "127.0.0.1:0"))
+		tr := &quic.Transport{Conn: pktConn}
+		conn := common.Must2(tr.DialEarly(context.Background(), addr, tlsConf, quicConf))
+
+		defer pktConn.Close()
+		defer tr.Close()
+		defer conn.CloseWithError(0, "")
+
+		var buf [1500]byte
+		err := conn.SendDatagram(buf[:])
+		var qErr *quic.DatagramTooLargeError
+		if !errors.As(err, &qErr) || qErr.MaxDatagramPayloadSize != 1197 {
+			t.Error(err)
+		}
+		if server := <-recv; server != 1197 {
+			t.Error(server)
+		}
+	})
+
+	cancel()
 }
