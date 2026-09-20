2184fadcd03726eb792a133bc220a3776d8e5023
Жора Змейкин <me@katze-942.ru>
2026-09-20 16:00:03 +0400

    Hysteria: fix UDP DATAGRAM truncation with ChromeParrot

--- transport/internet/hysteria/conn.go
+++ transport/internet/hysteria/conn.go
@@ -104,6 +104,9 @@ func (c *InterConn) Update() {
 func (c *InterConn) Read(p []byte) (int, error) {
 	b, ok := <-c.ch
 	if ok {
+		if len(p) < len(b) {
+			return 0, io.ErrShortBuffer
+		}
 		c.Update()
 		return copy(p, b), nil
 	}
